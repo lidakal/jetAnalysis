23340b4994e18a39660343429d94476edef54925
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-03-25 13:27:40 +0100

    pt2dscan ratio macros and plots

--- myMacros/pt2dscan/make_plots.C
+++ myMacros/pt2dscan/make_plots.C
@@ -1,19 +1,27 @@
 #include "draw_pt2dscan.C"
 #include "draw_pt2dscan_GSP.C"
+#include "draw_pt2dscan_ratio.C"
+#include "draw_pt2dscan_GSP_ratio.C"
 #include <initializer_list>
     
 int make_plots() 
 {
     for (auto qtype : {'B', 'C', 'L'}) {
+        /*
         draw_pt2dscan(qtype, true, true);
         draw_pt2dscan(qtype, true, false);
         draw_pt2dscan(qtype, false, true);
         draw_pt2dscan(qtype, false, false);
+        */
+        draw_pt2dscan_ratio(qtype, true, true);
+        draw_pt2dscan_ratio(qtype, true, false);
+        draw_pt2dscan_ratio(qtype, false, true);
+        draw_pt2dscan_ratio(qtype, false, false);
         
         if (qtype == 'L') {
             continue;
         }
-        
+        /*
         draw_pt2dscan_GSP(qtype, true, true, true);
         draw_pt2dscan_GSP(qtype, true, false, true);
         draw_pt2dscan_GSP(qtype, true, true, false);
@@ -23,6 +31,16 @@ int make_plots()
         draw_pt2dscan_GSP(qtype, false, false, true);
         draw_pt2dscan_GSP(qtype, false, true, false);
         draw_pt2dscan_GSP(qtype, false, false, false);
+        */
+        draw_pt2dscan_GSP_ratio(qtype, true, true, true);
+        draw_pt2dscan_GSP_ratio(qtype, true, false, true);
+        draw_pt2dscan_GSP_ratio(qtype, true, true, false);
+        draw_pt2dscan_GSP_ratio(qtype, true, false, false);
+        
+        draw_pt2dscan_GSP_ratio(qtype, false, true, true);
+        draw_pt2dscan_GSP_ratio(qtype, false, false, true);
+        draw_pt2dscan_GSP_ratio(qtype, false, true, false);
+        draw_pt2dscan_GSP_ratio(qtype, false, false, false);
     }
     return 0;
 }
