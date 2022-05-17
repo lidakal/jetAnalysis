e4063adad47af7323ebd70728625d6f8cd46015c
lidakal <lethakal@gmail.com>
2022-05-17 15:39:05 +0200

    add new plot to projection and change normalisation

--- myMacros/pt2dscan/projections1d/draw_projections1d.C
+++ myMacros/pt2dscan/projections1d/draw_projections1d.C
@@ -17,25 +17,31 @@ void draw_projections1d()
     std::string fname_chargedSJ_reco = "~/rootFiles/chargedSJ_new_noGSP_reco.root"; // ljet reco, bjet reco no merge
 
     std::string fname_merged_ref = "~/rootFiles/chargedSJ_mergedSVtracks_gen_reco_noGSP_ref.root"; // bjet truth merge
-    std::string fname_merged_reco = "~/rootFiles/chargedSJ_mergedSVtracks_gen_reco_noGSP_reco.root"; // bjet reco merge
+    std::string fname_merged_reco = "~/rootFiles/chargedSJ_mergedSVtracks_gen_reco_noGSP_reco.root"; // bjet reco merge only SV
+
+    std::string fname_merged_ip3dSig_reco = "~/rootFiles/aggregateB_ip3dSig_looserCut_noGSP_reco.root"; // bjet reco merge SV and ip3dSig   
 
     TFile *f_chargedSJ_ref = new TFile(fname_chargedSJ_ref.c_str());
     TH3D *hL3d_chargedSJ_ref = (TH3D *) f_chargedSJ_ref->Get("hL_rgkt");
-    hL3d_chargedSJ_ref->SetName("hL3d_chargedSJ_ref");
+    hL3d_chargedSJ_ref->SetName("hL3d_chargedSJ_ref"); // ljet truth
 
     TFile *f_chargedSJ_reco = new TFile(fname_chargedSJ_reco.c_str());
-    TH3D *hL3d_chargedSJ_reco = (TH3D *) f_chargedSJ_reco->Get("hL_rgkt");
+    TH3D *hL3d_chargedSJ_reco = (TH3D *) f_chargedSJ_reco->Get("hL_rgkt"); // ljet reco
     hL3d_chargedSJ_reco->SetName("hL3d_chargedSJ_reco");
-    TH3D *hB3d_chargedSJ_reco = (TH3D *) f_chargedSJ_reco->Get("hB_rgkt");
+    TH3D *hB3d_chargedSJ_reco = (TH3D *) f_chargedSJ_reco->Get("hB_rgkt"); // bjet reco no merge
     hB3d_chargedSJ_reco->SetName("hB3d_chargedSJ_reco");
 
     TFile *f_merged_ref = new TFile(fname_merged_ref.c_str());
-    TH3D *hB3d_merged_ref = (TH3D *) f_merged_ref->Get("hB_rgkt");
+    TH3D *hB3d_merged_ref = (TH3D *) f_merged_ref->Get("hB_rgkt"); // bjet truth merge 
     hB3d_merged_ref->SetName("hB3d_merged_ref");
 
     TFile *f_merged_reco = new TFile(fname_merged_reco.c_str());
-    TH3D *hB3d_merged_reco = (TH3D *) f_merged_reco->Get("hB_rgkt");
+    TH3D *hB3d_merged_reco = (TH3D *) f_merged_reco->Get("hB_rgkt"); // bjet reco merge only SV
     hB3d_merged_reco->SetName("hB3d_merged_reco");
+
+    TFile *f_merged_ip3dSig_reco = new TFile(fname_merged_ip3dSig_reco.c_str());
+    TH3D *hB3d_merged_ip3dSig_reco = (TH3D *) f_merged_ip3dSig_reco->Get("hB_rgkt"); // bjet reco merge SV and ip3dSig
+    hB3d_merged_ip3dSig_reco->SetName("hB3d_merged_ip3dSig_reco");
 
     Float_t lowpt[2] = {50., 80.};
     Float_t midpt[2] = {80., 120.};
@@ -57,6 +63,7 @@ void draw_projections1d()
         TH3D *hB3d_chargedSJ_reco_clone = (TH3D *) hB3d_chargedSJ_reco->Clone();
         TH3D *hB3d_merged_ref_clone = (TH3D *) hB3d_merged_ref->Clone();
         TH3D *hB3d_merged_reco_clone = (TH3D *) hB3d_merged_reco->Clone();
+        TH3D *hB3d_merged_ip3dSig_reco_clone = (TH3D *) hB3d_merged_ip3dSig_reco->Clone();
 
         // Set pt range
         hL3d_chargedSJ_ref_clone->GetZaxis()->SetRangeUser(ptmin, ptmax);
@@ -64,6 +71,7 @@ void draw_projections1d()
         hB3d_chargedSJ_reco_clone->GetZaxis()->SetRangeUser(ptmin, ptmax);
         hB3d_merged_ref_clone->GetZaxis()->SetRangeUser(ptmin, ptmax);
         hB3d_merged_reco_clone->GetZaxis()->SetRangeUser(ptmin, ptmax);
+        hB3d_merged_ip3dSig_reco_clone->GetZaxis()->SetRangeUser(ptmin, ptmax);
 
         // Make projections
         TH1D *hL1d_chargedSJ_ref = (TH1D *) hL3d_chargedSJ_ref_clone->Project3D(Form("x%d", i));
@@ -71,12 +79,14 @@ void draw_projections1d()
         TH1D *hB1d_chargedSJ_reco = (TH1D *) hB3d_chargedSJ_reco_clone->Project3D(Form("x%d", i));
         TH1D *hB1d_merged_ref = (TH1D *) hB3d_merged_ref_clone->Project3D(Form("x%d", i));
         TH1D *hB1d_merged_reco = (TH1D *) hB3d_merged_reco_clone->Project3D(Form("x%d", i));
+        TH1D *hB1d_merged_ip3dSig_reco = (TH1D *) hB3d_merged_ip3dSig_reco_clone->Project3D(Form("x%d", i));
 
-        normalise_histo(hL1d_chargedSJ_ref);
-        normalise_histo(hL1d_chargedSJ_reco);
-        normalise_histo(hB1d_chargedSJ_reco);
-        normalise_histo(hB1d_merged_ref);
-        normalise_histo(hB1d_merged_reco);
+        hL1d_chargedSJ_ref->Scale(1 / hL1d_chargedSJ_ref->Integral("width"));
+        hL1d_chargedSJ_reco->Scale(1 / hL1d_chargedSJ_reco->Integral("width"));
+        hB1d_chargedSJ_reco->Scale(1 / hB1d_chargedSJ_reco->Integral("width"));
+        hB1d_merged_ref->Scale(1 / hB1d_merged_ref->Integral("width"));
+        hB1d_merged_reco->Scale(1 / hB1d_merged_reco->Integral("width"));
+        hB1d_merged_ip3dSig_reco->Scale(1 / hB1d_merged_ip3dSig_reco->Integral("width"));
 
         Float_t ymax = std::max({hL1d_chargedSJ_ref->GetMaximum(), hL1d_chargedSJ_reco->GetMaximum(), 
                                 hB1d_chargedSJ_reco->GetMaximum(), hB1d_merged_ref->GetMaximum(), 
@@ -104,6 +114,7 @@ void draw_projections1d()
         hB1d_chargedSJ_reco->SetLineColor(3);
         hB1d_merged_ref->SetLineColor(4);
         hB1d_merged_reco->SetLineColor(6);
+        hB1d_merged_ip3dSig_reco->SetLineColor(7);
 
         TLegend *leg;
 		if (i == 2) {
@@ -118,7 +129,8 @@ void draw_projections1d()
         leg->AddEntry(hL1d_chargedSJ_reco, "inclusive jets, reco", "l");
         leg->AddEntry(hB1d_chargedSJ_reco, "bjets, reco", "l");
         leg->AddEntry(hB1d_merged_ref, "bjets, merged, truth", "l");
-        leg->AddEntry(hB1d_merged_reco, "bjets, merged, reco", "l");
+        leg->AddEntry(hB1d_merged_reco, "bjets, merged SV only, reco", "l");
+        leg->AddEntry(hB1d_merged_ip3dSig_reco, "bjets, merged SV and ip3dSig, reco", "l");
 
         crg->cd(i + 1);
         crg->cd(i + 1)->SetGrid(1);
@@ -128,6 +140,7 @@ void draw_projections1d()
         hB1d_chargedSJ_reco->Draw("hist same");
         hB1d_merged_ref->Draw("hist same");
         hB1d_merged_reco->Draw("hist same");
+        hB1d_merged_ip3dSig_reco->Draw("hist same");
 
         leg->Draw();
 
