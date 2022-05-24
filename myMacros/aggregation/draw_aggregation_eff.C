e06b1991a8c24e080cc5b310d83a3954318a03e4
lidakal <lethakal@gmail.com>
2022-05-24 10:35:59 +0200

    fix bug

--- myMacros/aggregation/draw_aggregation_eff.C
+++ myMacros/aggregation/draw_aggregation_eff.C
@@ -17,6 +17,7 @@ void draw_aggregation_eff()
     // ------------------- efficiency VS mistag rate ------------------------
 
 	TCanvas *c = new TCanvas("c", "", 800, 800);
+    c->Divide(2,1);
 
     TMultiGraph *mg = new TMultiGraph();
     TLegend *leg = new TLegend(0.2, 0.7, 0.5, 0.85);
@@ -53,18 +54,12 @@ void draw_aggregation_eff()
     std::string y1title = "pass selection | Not from B decay";
 	mg->SetTitle(Form("; %s; %s", x1title.c_str(), y1title.c_str()));
     
+    c->cd(1);
     mg->Draw("pla");
 	leg->Draw();
 
-	c->Draw();
-
-	std::string savename = "aggregation_eff_mistag.png";
-	c->Print(savename.c_str(), "png");
-
 
     //-------------------------- efficiency VS purity ---------------------
-
-    TCanvas *c_pur = new TCanvas("c_pur", "", 800, 800);
 
     TMultiGraph *mg_pur = new TMultiGraph();
     TLegend *leg_pur = new TLegend(0.2, 0.7, 0.5, 0.85);
@@ -73,7 +68,7 @@ void draw_aggregation_eff()
 	gStyle->SetLegendTextSize(15);
 
     TGraph *gr_pur_sel1 = make_graph_eff_pur(indir + fname, sel1, kFullCircle, kRed, leg_pur, label_sel1);
-    mg_pur->Add(gr_sel1);
+    mg_pur->Add(gr_pur_sel1);
 
     TGraph *gr_pur_sel6 = make_graph_eff_pur(indir + fname, sel6, kFullDiamond, kGreen, leg_pur, label_sel6);
     mg_pur->Add(gr_pur_sel6);
@@ -91,12 +86,14 @@ void draw_aggregation_eff()
     std::string y1title_pur = "From B decay | pass selection";
 	mg_pur->SetTitle(Form("; %s; %s", x1title_pur.c_str(), y1title_pur.c_str()));
     
+    c->cd(2);
     mg_pur->Draw("pla");
 	leg_pur->Draw();
 
-	c_pur->Draw();
+	c->Draw();
 
-	std::string savename_pur = "aggregation_eff_pur.png";
-	c_pur->Print(savename_pur.c_str(), "png");
+	std::string savename = "aggregation_eff_ROC.png";
+	c->Print(savename.c_str(), "png");
+
 
 }
