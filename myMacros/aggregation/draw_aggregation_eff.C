d767f3d152135d207907e36d0e19c274663ce565
lidakal <lethakal@gmail.com>
2022-05-24 10:28:39 +0200

    add purity graphs

--- myMacros/aggregation/draw_aggregation_eff.C
+++ myMacros/aggregation/draw_aggregation_eff.C
@@ -6,12 +6,15 @@
 #include "TStyle.h"
 #include "TCanvas.h"
 
-#include "utils.h" // make_graph()
+#include "utils.h" // make_graph_eff_mistag(), make_graph_eff_pur()
 
 void draw_aggregation_eff() 
 {
     std::string indir = "/home/llr/cms/kalipoliti/rootFiles/";
 	std::string fname = "aggregation_eff.root";
+
+
+    // ------------------- efficiency VS mistag rate ------------------------
 
 	TCanvas *c = new TCanvas("c", "", 800, 800);
 
@@ -23,27 +26,27 @@ void draw_aggregation_eff()
 
     std::string label_sel1 = "in SV";
     std::vector<Int_t> sel1 = {1};
-    TGraph *gr_sel1 = make_graph(indir + fname, sel1, kFullCircle, kRed, leg, label_sel1);
+    TGraph *gr_sel1 = make_graph_eff_mistag(indir + fname, sel1, kFullCircle, kRed, leg, label_sel1);
     mg->Add(gr_sel1);
 
     std::string label_sel6 = "|ip3dSig| > (12, 9, 6, 3)";
     std::vector<Int_t> sel6 = {6, 7, 8, 9};
-    TGraph *gr_sel6 = make_graph(indir + fname, sel6, kFullDiamond, kGreen, leg, label_sel6);
+    TGraph *gr_sel6 = make_graph_eff_mistag(indir + fname, sel6, kFullDiamond, kGreen, leg, label_sel6);
     mg->Add(gr_sel6);
     
 	std::string label_sel2 = "in SV || |ip3dSig| > (12, 9, 6, 3)";
     std::vector<Int_t> sel2 = {2, 3, 4, 5};
-    TGraph *gr_sel2 = make_graph(indir + fname, sel2, kFullTriangleUp, kBlue, leg, label_sel2);
+    TGraph *gr_sel2 = make_graph_eff_mistag(indir + fname, sel2, kFullTriangleUp, kBlue, leg, label_sel2);
     mg->Add(gr_sel2);
 
     std::string label_sel10 = "inSV && |ip3dSig| > (12, 9, 6, 3)";
     std::vector<Int_t> sel10 = {10, 11, 12, 13};
-    TGraph *gr_sel10 = make_graph(indir + fname, sel10, kFullTriangleDown, kViolet, leg, label_sel10);
+    TGraph *gr_sel10 = make_graph_eff_mistag(indir + fname, sel10, kFullTriangleDown, kViolet, leg, label_sel10);
     mg->Add(gr_sel10);
 
 	std::string label_sel14 = "(inSV && |ip3dSig| > 3) || (!inSV && |ip3dSig| > (9, 6, 3))";
 	std::vector<Int_t> sel14 = {14, 15, 16};
-	TGraph *gr_sel14 = make_graph(indir + fname, sel14, kOpenCircle, kOrange, leg, label_sel14);
+	TGraph *gr_sel14 = make_graph_eff_mistag(indir + fname, sel14, kOpenCircle, kOrange, leg, label_sel14);
 	mg->Add(gr_sel14);
 
     std::string x1title = "pass selection | From B decay";
@@ -55,6 +58,55 @@ void draw_aggregation_eff()
 
 	c->Draw();
 
-	std::string savename = "aggregation_eff_ROC.png";
+	std::string savename = "aggregation_eff_mistag.png";
 	c->Print(savename.c_str(), "png");
+
+
+    //-------------------------- efficiency VS purity ---------------------
+
+    TCanvas *c_pur = new TCanvas("cpur", "", 800, 800);
+
+    TMultiGraph *mg_pur = new TMultiGraph();
+    TLegend *leg_pur = new TLegend(0.2, 0.7, 0.5, 0.85);
+    leg_pur->SetBorderSize(0);
+    leg_pur->SetFillStyle(0);
+	gStyle->SetLegendTextSize(15);
+
+    std::string label_sel1 = "in SV";
+    std::vector<Int_t> sel1 = {1};
+    TGraph *gr_sel1 = make_graph_eff_pur(indir + fname, sel1, kFullCircle, kRed, leg_pur, label_sel1);
+    mg_pur->Add(gr_sel1);
+
+    std::string label_sel6 = "|ip3dSig| > (12, 9, 6, 3)";
+    std::vector<Int_t> sel6 = {6, 7, 8, 9};
+    TGraph *gr_sel6 = make_graph_eff_pur(indir + fname, sel6, kFullDiamond, kGreen, leg_pur, label_sel6);
+    mg_pur->Add(gr_sel6);
+    
+	std::string label_sel2 = "in SV || |ip3dSig| > (12, 9, 6, 3)";
+    std::vector<Int_t> sel2 = {2, 3, 4, 5};
+    TGraph *gr_sel2 = make_graph_eff_pur(indir + fname, sel2, kFullTriangleUp, kBlue, leg_pur, label_sel2);
+    mg_pur->Add(gr_sel2);
+
+    std::string label_sel10 = "inSV && |ip3dSig| > (12, 9, 6, 3)";
+    std::vector<Int_t> sel10 = {10, 11, 12, 13};
+    TGraph *gr_sel10 = make_graph_eff_pur(indir + fname, sel10, kFullTriangleDown, kViolet, leg_pur, label_sel10);
+    mg_pur->Add(gr_sel10);
+
+	std::string label_sel14 = "(inSV && |ip3dSig| > 3) || (!inSV && |ip3dSig| > (9, 6, 3))";
+	std::vector<Int_t> sel14 = {14, 15, 16};
+	TGraph *gr_sel14 = make_graph_eff_pur(indir + fname, sel14, kOpenCircle, kOrange, leg_pur, label_sel14);
+	mg_pur->Add(gr_sel14);
+
+    std::string x1title_pur = "pass selection | From B decay";
+    std::string y1title_pur = "From B decay | pass selection";
+	mg_pur->SetTitle(Form("; %s; %s", x1title_pur.c_str(), y1title_pur.c_str()));
+    
+    mg_pur->Draw("pla");
+	leg_pur->Draw();
+
+	c_pur->Draw();
+
+	std::string savename_pur = "aggregation_eff_pur.png";
+	c_pur->Print(savename_pur.c_str(), "png");
+
 }
--- myMacros/aggregation/utils.h
+++ myMacros/aggregation/utils.h
@@ -4,7 +4,37 @@
 #include "TLegend.h"
 #include "TStyle.h"
 
-TGraph * make_graph(std::string fname, std::vector<Int_t> v_selN, Style_t markerStyle, Color_t markerColor, TLegend *leg, std::string label)
+TGraph * make_graph_eff_mistag(std::string fname, std::vector<Int_t> v_selN, Style_t markerStyle, Color_t markerColor, TLegend *leg, std::string label)
+{
+    TFile *fin = new TFile(fname.c_str());
+    TGraph *gr = new TGraph();
+ 
+    for (Int_t selN : v_selN) {
+        TH1D *h = (TH1D *) fin->Get(Form("heff_sel%d", selN));
+
+        Float_t totalFromB = h->GetBinContent(1, 1) + h->GetBinContent(1, 2);
+        Float_t totalNotFromB = h->GetBinContent(2, 1) + h->GetBinContent(2, 2);
+
+        Float_t passFromB = h->GetBinContent(1, 1); 
+        Float_t passNotFromB = h->GetBinContent(2, 1);
+
+        Float_t x = passFromB / totalFromB; // efficiency
+        Float_t y = passNotFromB / totalNotFromB; // mistag rate
+
+        gr->SetPoint(gr->GetN(), x, y);
+    }
+
+    gr->SetMarkerStyle(markerStyle);
+    gr->SetMarkerColor(markerColor);
+
+    gr->SetLineStyle(kDashed);
+    gr->SetLineColor(markerColor);
+
+    leg->AddEntry(gr, label.c_str(), "pl");
+    return gr;
+}
+
+TGraph * make_graph_eff_pur(std::string fname, std::vector<Int_t> v_selN, Style_t markerStyle, Color_t markerColor, TLegend *leg, std::string label)
 {
     TFile *fin = new TFile(fname.c_str());
     TGraph *gr = new TGraph();
@@ -18,8 +48,8 @@ TGraph * make_graph(std::string fname, std::vector<Int_t> v_selN, Style_t marker
         Float_t passFromB = h->GetBinContent(1, 1);
         Float_t passNotFromB = h->GetBinContent(2, 1);
 
-        Float_t x = passFromB / totalFromB;
-        Float_t y = passNotFromB / totalNotFromB;
+        Float_t x = passFromB / totalFromB; // efficiency
+        Float_t y = passFromB / (passFromB + passNotFromB); // purity 
 
         gr->SetPoint(gr->GetN(), x, y);
     }
