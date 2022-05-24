98a0c9c65fb040441316cacdabf76202a81e45c3
lidakal <lethakal@gmail.com>
2022-05-24 11:55:49 +0200

    difference between true b jets and btag jets in rg distribution

---
+++ myMacros/bTagEff/draw_truth_btag_diff.C
@@ -0,0 +1,60 @@
+#include "TFile.h"
+#include "TH1D.h"
+#include "TStyle.h"
+#include "THStack.h"
+#include "TPaveText.h"
+#include "TLegend.h"
+
+#include "utils.h" // do_rg_projection()
+
+void draw_truth_btag_diff()
+{
+    std::string fname = "/home/llr/cms/kalipoliti/rootFiles/aggregateB_ip3dSig_looserCut_ref.root";
+    //std::string fname_noGSP = "/home/llr/cms/kalipoliti/rootFiles/aggregateB_ip3dSig_looserCut_noGSP_ref.root";
+
+    TCanvas *c = new TCanvas("c", "", 1000, 800);
+
+    TLegend *leg = new TLegend(0.5, 0.7, 0.8, 0.85);
+    leg->SetFillStyle(0);
+    leg->SetBorderSize(0);
+    gStyle->SetLegendTextSize(15);
+
+    THStack *hs = new THStack("hs", "");
+
+    Float_t ptrange[2] = {100., 150.};
+    TPaveText *info = new TPaveText(0.5, 0.4, 0.8, 0.6, "ndc");
+    info->AddText(Form("%.0f < p_{T, jet} < %.0f", ptrange[0], ptrange[1]));
+    info->SetTextSize(15);
+    info->SetFillStyle(0);
+    info->SetBorderSize(0);
+
+    TH1D *hB_rg = do_rg_projection(fname, "hB_rgkt", ptrange);
+    hB_rg->SetLineColor(1);
+    hB_rg->SetLineStyle(1);
+    leg->AddEntry("truth, true b-jets", "l");
+    hs->Add(hB_rg);
+
+    TH1D *hB_rg_dynKt = do_rg_projection(fname, "hB_rgkt_dynKt", ptrange);
+    hB_rg_dynKt->SetLineColor(1);
+    hB_rg_dynKt->SetLineStyle(2);
+    leg->AddEntry("truth, true b-jets, dynKt", "l");
+    hs->Add(hB_rg_dynKt);
+
+    TH1D *hBtag_rg = do_rg_projection(fname, "hBtag_rgkt", ptrange);
+    hBtag_rg->SetLineColor(2);
+    hBtag_rg->SetLineStyle(1);
+    leg->AddEntry("truth, tagged b-jets", "l");
+    hs->Add(hBtag_rg);
+
+    TH1D *hBtag_rg_dynKt = do_rg_projection(fname, "hBtag_rgkt_dynKt", ptrange);
+    hBtag_rg->SetLineColor(2);
+    hBtag_rg_dynKt->SetLineStyle(2);
+    leg->AddEntry("truth, tagged b-jets, dynKt", "l");
+    hs->Add(hBtag_rg_dynKt);
+
+    hs->Draw("nostack hist");
+    leg->Draw();
+    info->Draw();
+
+    c->Draw();
+}
