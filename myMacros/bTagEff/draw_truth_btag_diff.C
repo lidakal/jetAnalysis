30aee82bbcbfbd9bf56fffd898b4e00ee01aa320
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-05-25 12:28:35 +0200

    update plots, fix bugs

--- myMacros/bTagEff/draw_truth_btag_diff.C
+++ myMacros/bTagEff/draw_truth_btag_diff.C
@@ -16,26 +16,48 @@ void draw_truth_btag_diff()
     TCanvas *c = new TCanvas("c", "", 1800, 800);
     c->Divide(2, 1);
 
+	THStack *hs = new THStack("hs", "");
+	THStack *hs_ratio = new THStack("hs_ratio", "");
+
+	std::string x1title = "ln(1/R_{g})";
+	std::string y1title = "1/N_{2-prong jets} dN/dln(1/R_{g})";
+	std::string y2title = "N_{true b-jets} / N_{tagged b-jets}";
+
+	hs->SetTitle(Form("; %s; %s", x1title.c_str(), y1title.c_str()));
+	hs_ratio->SetTitle(Form("; %s; %s", x1title.c_str(), y2title.c_str()));
+
+	TLegend *leg = new TLegend(0.4, 0.6, 0.85, 0.85);
+    leg->SetFillStyle(0);
+    gStyle->SetLegendTextSize(15);
+
+    TLegend *leg_ratio = new TLegend(0.5, 0.4, 0.8, 0.6);
+	leg_ratio->SetFillStyle(0);
+	leg_ratio->SetBorderSize(0);
+
     Float_t ptrange1[2] = {100., 150.};
-    draw_rg_projection(c, ptrange1);
+    draw_rg_projection(c, hs, leg, hs_ratio, leg_ratio, ptrange1);
 
     Float_t ptrange2[2] = {200., 250.};
-    draw_rg_projection(c, ptrange2);
+    draw_rg_projection(c, hs, leg, hs_ratio, leg_ratio, ptrange2);
 
     Float_t ptrange3[2] = {300., 350.};
-    draw_rg_projection(c, ptrange3);
+    draw_rg_projection(c, hs, leg, hs_ratio, leg_ratio, ptrange3);
 
-    TPaveText *info = new TPaveText(0.2, 0.3, 0.4, 0.45, "ndc");
-	info->AddText("Aggregated pseudo-B's");
+    TPaveText *info = new TPaveText(0.2, 0.25, 0.5, 0.4, "ndc");
+	info->AddText("truth level, aggregated pseudo-B's");
     info->SetTextSize(15);
     info->SetFillStyle(0);
     info->SetBorderSize(0);
 
     c->cd(1);
+	hs->Draw("nostack hist");
+	leg->Draw();
     info->Draw();
 
     c->cd(2);
-	info_ratio->Draw();
+	hs_ratio->Draw("nostack hist");
+	leg_ratio->Draw();
+	info->Draw();
 
     c->Draw();
 
--- myMacros/bTagEff/utils.h
+++ myMacros/bTagEff/utils.h
@@ -28,7 +28,7 @@ TH1D * do_rg_projection(std::string fname, std::string hname, Float_t *ptrange)
     return h1d;
 }
 
-void draw_rg_projection(TCanvas *c, Float_t *ptrange)
+void draw_rg_projection(TCanvas *c, THStack *hs, TLegend *leg, THStack *hs_ratio, TLegend *leg_ratio, Float_t *ptrange)
 {
     Float_t ptmin = ptrange[0];
     Float_t ptmax = ptrange[1];
@@ -36,57 +36,27 @@ void draw_rg_projection(TCanvas *c, Float_t *ptrange)
     static Int_t lcolor = 1;
     static Int_t lstyle = 1;
 
-    Float_t x1leg = 0.6, x2leg = 0.8;
-    static Float_t y1leg = 0.7, y2leg = 0.85;
-
-    TLegend *leg = new TLegend(x1leg, y1leg, x2leg, y2leg);
-    leg->SetFillStyle(0);
-    leg->SetBorderSize(1);
-    gStyle->SetLegendTextSize(15);
-
-    TLegend *leg_ratio = (TLegend *) leg->Clone();
-    leg_ratio->SetBorderSize(0);
-
-    THStack *hs = new THStack("hs", "");
-
     std::string fname = "/home/llr/cms/kalipoliti/rootFiles/aggregateB_ip3dSig_looserCut_ref.root";
 
     TH1D *hB_rg = do_rg_projection(fname, "hB_rgkt", ptrange);
     hB_rg->SetLineColor(lcolor);
     hB_rg->SetLineStyle(1);
-    leg->AddEntry(hB_rg, "truth level, true b-jets", "l");
+    leg->AddEntry(hB_rg, Form("true b-jets, p_{T}^{jet} #in [%.0f, %.0f] (GeV)", ptmin, ptmax), "l");
     hs->Add(hB_rg);
 
     TH1D *hBtag_rg = do_rg_projection(fname, "hBtag_rgkt", ptrange);
     hBtag_rg->SetLineColor(lcolor);
     hBtag_rg->SetLineStyle(2);
-    leg->AddEntry(hBtag_rg, "truth level, tagged b-jets", "l");
+    leg->AddEntry(hBtag_rg, Form("tagged b-jets,  p_{T}^{jet} #in [%.0f, %.0f] (GeV)", ptmin, ptmax), "l");
     hs->Add(hBtag_rg);
-
-    std::string x1title = "ln(1/R_{g})";
-	std::string y1title = "1/N_{2-prong jets} dN/dln(1/R_{g})";
-    std::string y2title = "N_{true b-jets} / N_{tagged b-jets}";
-
-	hs->SetTitle(Form("; %s; %s", x1title.c_str(), y1title.c_str()));
 
     // Create ratio histogram
     TH1D *h_ratio = (TH1D *) hB_rg->Clone();
     h_ratio->Divide(hBtag_rg);
     h_ratio->SetLineColor(lcolor);
-    leg_ratio->AddEntry(h_ratio, Form("%0.f < p_{T}^{jet} < %0.f (GeV)", ptmin, ptmax));
+    leg_ratio->AddEntry(h_ratio, Form("%0.f < p_{T}^{jet} < %0.f (GeV)", ptmin, ptmax), "l");
 
-    h_ratio->SetTitle(Form("; %s; %s", x1title.c_str(), y2title.c_str()));
-
-    c->cd(1);
-    hs->Draw("nostack hist same");
-    leg->SetHeader(Form("p_{T} #in [%.0f, %.0f] GeV", ptmin, ptmax));
-    leg->Draw();
-
-    c->cd(2);
-    h_ratio->Draw("hist same");
-    leg_ratio->Draw();
+	hs_ratio->Add(h_ratio);
 
     lcolor++;
-    y1leg -= 0.2;
-    y2leg -= 0.2;
 }
