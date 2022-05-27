019012555159963f72f13c16a5854cc4578cdf19
lidakal <lethakal@gmail.com>
2022-05-27 10:08:08 +0200

    clean drawing of histograms

---
+++ myMacros/pt2dscan/HistDrawer_pt2dscan.h
@@ -0,0 +1,78 @@
+#include "TFile.h"
+#include "TH3D.h"
+#include "TH2D.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TPaveText.h"
+
+// Class to load a 3D histo and create the projections
+class HistDrawer_pt2dscan
+{
+    public:
+        TH3D *h3d; 
+
+        // ------ Constructors -------
+        HistDrawer_pt2dscan() {};
+        HistDrawer_pt2dscan(std::string fname, std::string hname);
+
+        TH2D * do_rgkt_projection(Float_t *ptrange, std::string hname);
+        TH1D * do_rg_projection(Float_t *ptrange, std::string hname);
+};
+
+HistDrawer_pt2dscan::HistDrawer_pt2dscan(std::string fname, std::string hname)
+{
+    TFile *fin = new TFile(fname.c_str());
+    h3d = (TH3D *) fin->Get(hname.c_str());
+}
+
+TH2D * HistDrawer_pt2dscan::do_rgkt_projection(Float_t *ptrange, std::string hname)
+{
+    TH3D *htemp = (TH3D *) h3d->Clone();
+    htemp->GetZaxis()->SetRangeUser(ptrange[0], ptrange[1]);
+
+    TH2D *h2d = (TH2D *) htemp->Project3D("xy");
+    h2d->Scale(1. / h2d->Integral("width"));
+
+    std::string xtitle = "ln(1/R_{g})";
+    std::string ytitle = "ln(k_{T})";
+
+    h2d->GetXaxis()->SetTitle(xtitle.c_str());
+    h2d->GetYaxis()->SetTitle(ytitle.c_str());
+
+    return h2d;
+}
+
+TH1D * HistDrawer_pt2dscan::do_rg_projection(Float_t *ptrange, std::string hname)
+{
+    TH3D *htemp = (TH3D *) h3d->Clone();
+    htemp->GetZaxis()->SetRangeUser(ptrange[0], ptrange[1]);
+
+    TH1D *h1d = (TH1D *) htemp->Project3D("x");
+    h1d->Scale(1. / h1d->Integral("width"));
+
+    std::string xtitle = "ln(1/R_{g})";
+    std::string ytitle = "1 / N_{2-prog jets} dN/dln(1/R_{g})";
+
+    h1d->GetXaxis()->SetTitle(xtitle.c_str());
+    h1d->GetYaxis()->SetTitle(ytitle.c_str());
+
+    return h1d;
+}
+
+TH1D * HistDrawer_pt2dscan::do_kt_projection(Float_t *ptrange, std::string hname)
+{
+    TH3D *htemp = (TH3D *) h3d->Clone();
+    htemp->GetZaxis()->SetRangeUser(ptrange[0], ptrange[1]);
+
+    TH1D *h1d = (TH1D *) htemp->Project3D("y");
+    h1d->Scale(1. / h1d->Integral("width"));
+
+    std::string xtitle = "ln(k_{T})";
+    std::string ytitle = "1 / N_{2-prog jets} dN/dln(k_{T})";
+
+    h1d->GetXaxis()->SetTitle(xtitle.c_str());
+    h1d->GetYaxis()->SetTitle(ytitle.c_str());
+
+    return h1d;
+}
