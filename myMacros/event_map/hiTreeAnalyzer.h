1b226c793f12352cc4f75b4e8c65cb0d2c6d01c9
lidakal <lethakal@gmail.com>
2022-06-01 10:48:02 +0200

    add TreeAnalyzer for hi tree (gen particles)

---
+++ myMacros/event_map/hiTreeAnalyzer.h
@@ -0,0 +1,67 @@
+#include "TFile.h"
+#include "TTree.h"
+
+#include <vector>
+
+class hiTreeAnalyzer {
+    public :
+        TFile *fin;
+        TTree *hi;
+
+        Long64_t nentries;
+
+        // Declaration of leaf types
+        Int_t           mult;
+        std::vector<float>   *pt = 0;
+        std::vector<float>   *eta = 0;
+        std::vector<float>   *phi = 0;
+        std::vector<int>     *pdg = 0;
+        std::vector<int>     *sta = 0;
+
+        hiTreeAnalyzer() {};
+        hiTreeAnalyzer(std::string fname, bool init = true);
+
+        void Init();
+        void GetEntry(Long64_t ient);
+        void SetBranchStatus(std::string branchName, Int_t status);
+    	void SetBranchStatus(std::vector<std::string> branchNames, Int_t status);
+};
+
+hiTreeAnalyzer::hiTreeAnalyzer(std::string fname, bool init = true) 
+{
+   fin = new TFile(fname.c_str());
+   
+   if (init) {
+      Init();
+   }
+}
+
+void hi::Init()
+{
+   hi->SetBranchAddress("mult", &mult);
+   hi->SetBranchAddress("pt", &pt);
+   hi->SetBranchAddress("eta", &eta);
+   hi->SetBranchAddress("phi", &phi);
+   hi->SetBranchAddress("pdg", &pdg);
+   hi->SetBranchAddress("sta", &sta);
+
+   nentries = hi->GetEntries();
+}
+
+void hiTreeAnalyzer::GetEntry(Long64_t ient)
+{
+    hi->GetEntry(ient);
+}
+
+void hiTreeAnalyzer::SetBranchStatus(std::string branchName, Int_t status)
+{
+    hi->SetBranchStatus(branchName.c_str(), status);
+}
+
+void hiTreeAnalyzer::SetBranchStatus(std::vector<std::string> branchNames, Int_t status)
+{
+    for (std::string branchName : branchNames) {
+        hi->SetBranchStatus(branchName.c_str(), status);
+    }
+}
+
