a70817ed0f8bafb0c61793fa91a04e3ed3451992
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-06-09 09:52:31 +0200

    fixed bugs in aggregation, highest efficiency, new plots

--- myMacros/pt2dscan/aggregateB_highestEfficiency/draw_aggregateB_highestEfficiency.C
+++ myMacros/pt2dscan/aggregateB_highestEfficiency/draw_aggregateB_highestEfficiency.C
@@ -4,5 +4,5 @@ void draw_aggregateB_highestEfficiency()
 {          
     std::string fname = "aggregateB_highestEfficiency";
     HistDrawer_pt2dscan HD(fname);
-    HD.draw_pt2dscan(true);
+    HD.draw_pt2dscan(false);
 }
