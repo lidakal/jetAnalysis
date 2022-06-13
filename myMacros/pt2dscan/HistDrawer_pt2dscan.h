fab255d090c8a3cd303f4fc84c8b7e27d7ac3a71
lidakal <lethakal@gmail.com>
2022-06-13 11:44:57 +0200

    add zg 1d projection plotter

--- myMacros/pt2dscan/HistDrawer_pt2dscan.h
+++ myMacros/pt2dscan/HistDrawer_pt2dscan.h
@@ -210,3 +210,102 @@ void HistDrawer_pt2dscan::draw_rg_projection(bool GSPincl)
 	std::string savename = "rg_projections_1ptBin" + noGSP + ".png";
 	c->Print(savename.c_str());
 }
+
+void HistDrawer_pt2dscan::draw_zg_projection(bool GSPincl)
+{
+    std::string noGSP = "";
+    if (!GSPincl) {
+        noGSP += "_noGSP";
+    }
+
+    // These do not change
+    std::string fname_chargedSJ_qcdMC_ref = "~/rootFiles/chargedSJ_new_qcdMC_ref.root"; // ljet truth
+    std::string fname_chargedSJ_qcdMC_reco = "~/rootFiles/chargedSJ_new_qcdMC_reco.root"; // ljet reco
+    std::string fname_chargedSJ_bJetMC_reco = "~/rootFiles/chargedSJ_new_bJetMC" + noGSP + "_reco.root"; // bjet reco no merge
+	std::string fname_merged_ref = "~/rootFiles/aggregateB_truthInfo_fixedBugs" + noGSP + "_ref.root"; // bjet truth merge
+    std::string fname_merged_truth_reco = "~/rootFiles/aggregateB_truthInfo_fixedBugs" + noGSP + "_reco.root"; // bjet reco merge with truth info
+
+    // New selections can be added here
+    std::string fname_merged_reco = "~/rootFiles/chargedSJ_mergedSVtracks_gen_reco" + noGSP + "_reco.root"; // bjet reco merge only SV
+    std::string fname_merged_highestEfficiency_reco = "~/rootFiles/aggregateB_highestEfficiency" + noGSP + "_reco.root"; // bjet reco merge (inSV || (|ip3dSig| > 3))
+
+    // Load histograms
+    Float_t ptrange[2] = {100., 150.};
+    THStack *hs = new THStack("hs", "");
+    TLegend *leg = new TLegend(0.5, 0.6, 0.8, 0.85);
+    gStyle->SetLegendTextSize(15);
+
+    HistLoader_pt2dscan HL_ljet_ref(fname_chargedSJ_qcdMC_ref, "hL_zgkt");
+    TH1D *h1d_ljet_ref = HL_ljet_ref.do_zg_projection(ptrange, "h1d_ljet_ref");
+    h1d_ljet_ref->SetLineColor(1);
+    h1d_ljet_ref->SetLineStyle(2);
+    leg->AddEntry(h1d_ljet_ref, "l-jets, truth", "l");
+    hs->Add(h1d_ljet_ref);
+
+    HistLoader_pt2dscan HL_ljet_reco(fname_chargedSJ_qcdMC_reco, "hL_zgkt");
+    TH1D *h1d_ljet_reco = HL_ljet_reco.do_zg_projection(ptrange, "h1d_ljet_reco");
+    h1d_ljet_reco->SetLineColor(2);
+    h1d_ljet_reco->SetLineStyle(2);
+    leg->AddEntry(h1d_ljet_reco, "l-jets, reco", "l");
+    hs->Add(h1d_ljet_reco);
+
+    HistLoader_pt2dscan HL_bjet_ref(fname_merged_ref, "hBtag_zgkt");
+    TH1D *h1d_bjet_ref = HL_bjet_ref.do_zg_projection(ptrange, "h1d_bjet_ref");
+    h1d_bjet_ref->SetLineColor(4);
+    h1d_bjet_ref->SetLineStyle(1);
+    leg->AddEntry(h1d_bjet_ref, "b-jets, truth, aggregated pseudo-B's", "l");
+    hs->Add(h1d_bjet_ref);
+
+    HistLoader_pt2dscan HL_bjet_reco(fname_chargedSJ_bJetMC_reco, "hBtag_zgkt");
+    TH1D *h1d_bjet_reco = HL_bjet_reco.do_zg_projection(ptrange, "h1d_bjet_reco");
+    h1d_bjet_reco->SetLineColor(6);
+    h1d_bjet_reco->SetLineStyle(1);
+    leg->AddEntry(h1d_bjet_reco, "b-jets, reco, no aggregation", "l");
+    hs->Add(h1d_bjet_reco);
+
+    HistLoader_pt2dscan HL_bjet_reco_truth(fname_merged_truth_reco, "hBtag_zgkt");
+    TH1D *h1d_bjet_reco_truth = HL_bjet_reco_truth.do_zg_projection(ptrange, "h1d_bjet_reco_truth");
+    h1d_bjet_reco_truth->SetLineColor(7);
+    h1d_bjet_reco_truth->SetLineStyle(1);
+    leg->AddEntry(h1d_bjet_reco_truth, "b-jets, reco, aggregation with truth info", "l");
+    hs->Add(h1d_bjet_reco_truth);
+
+    HistLoader_pt2dscan HL_bjet_reco_SV(fname_merged_reco, "hBtag_zgkt");
+    TH1D *h1d_bjet_reco_SV = HL_bjet_reco_SV.do_zg_projection(ptrange, "h1d_bjet_reco_SV");
+    h1d_bjet_reco_SV->SetLineColor(8);
+    h1d_bjet_reco_SV->SetLineStyle(1);
+    leg->AddEntry(h1d_bjet_reco_SV, "b-jets, reco, aggregation if inSV", "l");
+    hs->Add(h1d_bjet_reco_SV);
+
+    HistLoader_pt2dscan HL_bjet_reco_highestEfficiency(fname_merged_highestEfficiency_reco, "hBtag_zgkt");
+    TH1D *h1d_bjet_reco_highestEfficiency = HL_bjet_reco_highestEfficiency.do_zg_projection(ptrange, "h1d_bjet_reco_highestEfficiency");
+    h1d_bjet_reco_highestEfficiency->SetLineColor(28);
+    h1d_bjet_reco_highestEfficiency->SetLineStyle(1);
+    leg->AddEntry(h1d_bjet_reco_highestEfficiency, "b-jets, reco, aggregation if (inSV || (|ip3dSig| > 3))", "l");
+    hs->Add(h1d_bjet_reco_highestEfficiency);    
+
+	std::string xtitle = "z_{g}";
+	std::string ytitle = "1/N_{2-prong jets} dN/dz_{g}";
+	hs->SetTitle(Form("; %s; %s", xtitle.c_str(), ytitle.c_str()));
+
+    TCanvas *c = new TCanvas("c", "", 1200, 800);
+    hs->Draw("nostack hist");
+    leg->Draw();
+
+    TPaveText *info = new TPaveText(0.2, 0.17, 0.45, 0.28, "ndc");
+    info->SetBorderSize(1);
+    info->SetFillColor(0);
+    info->SetTextSize(15);
+    info->AddText(Form("%.0f < p_{T}^{jet} < %0.f (GeV)", ptrange[0], ptrange[1]));
+    if (!GSPincl) {
+	  //info->AddText("truth doesn't pass b-tag, NO GSP");
+	  info->AddText("all b-jets pass b-tag, NO GSP");
+    }
+	info->AddText("ln(k_{T}) > 0");
+    info->Draw();
+
+    c->Draw();
+
+	std::string savename = "zg_projections_1ptBin" + noGSP + ".png";
+	c->Print(savename.c_str());
+}
