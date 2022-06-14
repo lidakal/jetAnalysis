58d6fa58764419820ffde203e6d6a966084a98f1
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-06-14 14:35:05 +0200

    pt2dscan for ip3dSigOnly selection

--- myMacros/pt2dscan/aggregateB_ip3dSigOnly/plot_aggregateB_ip3dSigOnly.C
+++ myMacros/pt2dscan/aggregateB_ip3dSigOnly/plot_aggregateB_ip3dSigOnly.C
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 
 
-void plot_aggregateB_SV_ip3dSig_tight()
+void plot_aggregateB_ip3dSigOnly()
 {
-    std::string finname = "/data_CMS/cms/kalipoliti/bJetMC/aggregateB_SV_ip3dSig_tight/merged_HiForestAOD.root";
-    std::string foutname = "~/rootFiles/aggregateB_SV_ip3dSig_tight";
+    std::string finname = "/data_CMS/cms/kalipoliti/bJetMC/aggregateB_ip3dSigOnly/merged_HiForestAOD.root";
+    std::string foutname = "~/rootFiles/aggregateB_ip3dSigOnly";
 
     TreeAnalyzer_pt2dscan TAref(finname, true);
-    //TAref.plot_pt2dscan(foutname, "ref", true);  // truth with GSP
-    //TAref.plot_pt2dscan(foutname, "ref", false);  // truth no GSP
+    TAref.plot_pt2dscan(foutname, "ref", true);  // truth with GSP
+    TAref.plot_pt2dscan(foutname, "ref", false);  // truth no GSP
 
     TreeAnalyzer_pt2dscan TAreco(finname, true);
-    TAreco.plot_pt2dscan(foutname, "reco", true);  // reco with GSP
-    TAreco.plot_pt2dscan(foutname, "reco", false);  // reco no GSP
+    //TAreco.plot_pt2dscan(foutname, "reco", true);  // reco with GSP
+    //TAreco.plot_pt2dscan(foutname, "reco", false);  // reco no GSP
 }
