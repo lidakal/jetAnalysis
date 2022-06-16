97d5907c995ae946a4176cce64d9f5e153b0fe93
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-06-16 13:07:45 +0200

    fix legend entries' descriptions

--- myMacros/pt2dscan/projections1d/draw_projections1d_1ptBin.C
+++ myMacros/pt2dscan/projections1d/draw_projections1d_1ptBin.C
@@ -6,6 +6,6 @@ void draw_projections1d_1ptBin(bool GSPincl = false)
     Float_t ktrange[] = {-5., 5.};
 
     HistDrawer_pt2dscan HD;
-    //HD.draw_1d_projection(GSPincl, "rg", ptrange, ktrange);
+    HD.draw_1d_projection(GSPincl, "rg", ptrange, ktrange);
     HD.draw_1d_projection(GSPincl, "zg", ptrange, ktrange);
 }
