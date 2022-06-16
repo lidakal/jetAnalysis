78f31994c650945e229182b0c4b34f708d27fa67
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-06-16 12:47:08 +0200

    update appearence of rg zg 1 pt bin distribution

--- myMacros/pt2dscan/projections1d/draw_projections1d_1ptBin.C
+++ myMacros/pt2dscan/projections1d/draw_projections1d_1ptBin.C
@@ -2,7 +2,10 @@
 
 void draw_projections1d_1ptBin(bool GSPincl = false)
 {
+    Float_t ptrange[] = {50., 80.};
+    Float_t ktrange[] = {-5., 5.};
+
     HistDrawer_pt2dscan HD;
-    //HD.draw_1d_projection(GSPincl, "rg");
-    HD.draw_1d_projection(GSPincl, "zg");
+    //HD.draw_1d_projection(GSPincl, "rg", ptrange, ktrange);
+    HD.draw_1d_projection(GSPincl, "zg", ptrange, ktrange);
 }
