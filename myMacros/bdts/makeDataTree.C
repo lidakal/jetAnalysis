6642994a6363b0b24c121ba8ef615d77244ed27b
Lida KALIPOLITI <kalipoliti@polui07.in2p3.fr>
2022-07-29 13:42:51 +0200

    update makeDataTree.C

--- myMacros/bdts/makeDataTree.C
+++ myMacros/bdts/makeDataTree.C
@@ -5,14 +5,15 @@
 
 void makeDataTree()
 {
-    std::string finname = "/home/llr/cms/kalipoliti/CMSSW_9_4_10/src/HeavyIonsAnalysis/JetAnalysis/test/HiForestAOD.root";
+    std::string finname = "/data_CMS/cms/kalipoliti/bJetMC/trackTaggingTraining/merged_HiForestAOD.root";
     TreeAnalyzer ta(finname, true);
 
     // Activate branches of interest
 
     std::vector<std::string> activeBranches = {"nref", "jteta", "jtpt", "jtHadFlav", "jtNbHad", 
                                                 "jtDiscDeepFlavourB", "jtDiscDeepFlavourBB", "jtDiscDeepFlavourLEPB", 
-                                                "nselIPtrk", "ipEta", "ipPhi", "ipMatchStatus", "ip3dSig", "inSV", 
+                                                "nselIPtrk", "ipEta", "ipPhi", "ipMatchStatus", "ip3dSig", "ipInSV", 
+                                                "ipSvtxdls", "ipSvtxdls2d", "ipSvtxm", "ipSvtxmcorr",
                                                 "svtxTrEta", "svtxTrPhi", "svtxdls",
                                                 "weight"};
 
@@ -20,24 +21,38 @@ void makeDataTree()
     ta.SetBranchStatus(activeBranches, 1);
 
     // Create new file to store signal tree
-    std::string foutName = "tmva_data.root";
+    std::string foutName = "tmva_data_complex.root";
     TFile *fout = new TFile(foutName.c_str(), "recreate");
     std::cout << "(Re)creating " << foutName << " file." << std::endl;
 
     TTree *TreeS = new TTree("TreeS", "Signal tree for TMVA");
-    TTree *TreeB = new TTree("TreeB", "Background tree for TMVA");
+    // TTree *TreeB = new TTree("TreeB", "Background tree for TMVA");
 
     Float_t ip3dSig;
-    Int_t inSV;
+    Int_t ipInSV;
+    Float_t ipSvtxdls;
+    Float_t ipSvtxdls2d;
+    Float_t ipSvtxm;
+    Float_t ipSvtxmcorr;
     Float_t weight;
 
     TreeS->Branch("ip3dSig", &ip3dSig, "ip3dSig/F");
-    TreeS->Branch("inSV", &inSV, "inSV/I");
+    TreeS->Branch("ipInSV", &ipInSV, "ipInSV/I");
+    TreeS->Branch("ipSvtxdls", &ipSvtxdls, "ipSvtxdls/F");
+    TreeS->Branch("ipSvtxdls2d", &ipSvtxdls2d, "ipSvtxdls2d/F");
+    TreeS->Branch("ipSvtxm", &ipSvtxm, "ipSvtxm/F");
+    TreeS->Branch("ipSvtxmcorr", &ipSvtxmcorr, "ipSvtxmcorr/F");
     TreeS->Branch("weight", &weight, "weight/F");
 
-    TreeB->Branch("ip3dSig", &ip3dSig, "ip3dSig/F");
-    TreeB->Branch("inSV", &inSV, "inSV/I");
-    TreeB->Branch("weight", &weight, "weight/F");
+    // TreeB->Branch("ip3dSig", &ip3dSig, "ip3dSig/F");
+    // TreeB->Branch("ipInSV", &ipInSV, "ipInSV/I");
+    // TreeB->Branch("ipSvtxdls", &ipSvtxdls, "ipSvtxdls/F");
+    // TreeB->Branch("ipSvtxdls2d", &ipSvtxdls2d, "ipSvtxdls2d/F");
+    // TreeB->Branch("ipSvtxm", &ipSvtxm, "ipSvtxm/F");
+    // TreeB->Branch("ipSvtxmcorr", &ipSvtxmcorr, "ipSvtxmcorr/F");
+    // TreeB->Branch("weight", &weight, "weight/F");
+
+    Long64_t counts = 0;
 
     for (Long64_t ient = 0; ient < ta.nentries; ient++) {
             
@@ -46,7 +61,7 @@ void makeDataTree()
         //if (ient > 10) break;
             
         // Show progress
-        if (ient % 100 == 0) {
+        if (ient % 1000000 == 0) {
             std::cout << "i = " << ient << std::endl;
         }
 
@@ -57,7 +72,9 @@ void makeDataTree()
 
         for (Int_t ijet = 0; ijet < ta.nref; ijet++) {
             // bjet & eta cut 
+            // ignoring the btagging lowers the efficiency
             bool isBjet = (ta.jtDiscDeepFlavourB[ijet] + ta.jtDiscDeepFlavourBB[ijet] + ta.jtDiscDeepFlavourLEPB[ijet]) > 0.9;
+            //bool isBjet = true; // forget about btagging for a bit 
 
             if ((!isBjet) || (std::abs(ta.jteta[ijet]) > 2)) {
                 itrackOffset += ta.nselIPtrk[ijet];
@@ -75,23 +92,38 @@ void makeDataTree()
                 // Get track properties
                 
                 ip3dSig = ta.ip3dSig[itrackOffset + itrack];
-                inSV = ta.inSV[itrackOffset + itrack];
+                ipInSV = ta.ipInSV[itrackOffset + itrack];
                 Int_t sta = ta.ipMatchStatus[itrackOffset + itrack];
                 //Int_t signal = int(sta >= 100);
 
+                ipSvtxdls = ta.ipSvtxdls[itrackOffset + itrack];
+                ipSvtxdls2d = ta.ipSvtxdls2d[itrackOffset + itrack];
+                ipSvtxm = ta.ipSvtxm[itrackOffset + itrack];
+                ipSvtxmcorr = ta.ipSvtxmcorr[itrackOffset + itrack];
+
                 weight = ta.weight;
+
+                if (ipInSV == 0) continue;
 
                 if (sta >= 100) {
                     TreeS->Fill();
+                    counts++;
                 } else {
-                    TreeB->Fill();
+                    // TreeB->Fill();
                 }
 
             } // end track loop
         } // end jet loop
     } // end entry loop
+
+    std::cout << "Before writing" << std::endl;
+    std::cout << "TreeS->GetEntries() : " << TreeS->GetEntries() << std::endl;
+    std::cout << "counts : " << counts << std::endl;
     TreeS->Write();
-    TreeB->Write();
+    std::cout << "After writing" << std::endl;
+    std::cout << "TreeS->GetEntries() : " << TreeS->GetEntries() << std::endl;
+    //TreeB->Write();
+    //std::cout << "fout->ls() : " << fout->ls() << std::endl;
     fout->Close();
     delete fout;
                 
