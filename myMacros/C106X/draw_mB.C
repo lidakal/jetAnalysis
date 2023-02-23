f27e4e3f351ee016cc3d768f38d3aced85edd5e5
Lida Kalipoliti <lethakal@gmail.com>
2023-02-23 14:35:07 +0100

    update mB plot

--- myMacros/C106X/draw_mB.C
+++ myMacros/C106X/draw_mB.C
@@ -59,90 +59,90 @@ void draw_mB()
     // cTest->Draw();
 
     // ---- Load the histos
-    TString fdir = "./histos/";
-    TString label = "mB";
-    TString suffix_truth = "_truth";
-    TString fname_truth = label + suffix_truth + ".root";
-    TString rootf_truth = fdir + fname_truth;
+    TString indir = "./histos/";
+    // TString fdir = "./histos/";
+    // TString label = "mB";
+    // TString suffix_truth = "_truth";
+    // TString fname_truth = label + suffix_truth + ".root";
+    // TString rootf_truth = fdir + fname_truth;
 
-    TFile *fin_truth = new TFile(rootf_truth);
+    // TFile *fin_truth = new TFile(rootf_truth);
 
-    TH3D *hmB_truth_3d = (TH3D *)fin_truth->Get("hmBtag");
-    hmB_truth_3d->SetName(TString(hmB_truth_3d->GetName()) + "_truth");
+    // TH3D *hmB_truth_3d = (TH3D *)fin_truth->Get("hmBtag");
+    // hmB_truth_3d->SetName(TString(hmB_truth_3d->GetName()) + "_truth");
 
-    TString suffix_bdt = "_bdt29";
-    TString fname_bdt = label + suffix_bdt + ".root";
-    TString rootf_bdt = fdir + fname_bdt;
+    // TString suffix_bdt = "_bdt29";
+    TString label_aggrTMVA = "aggrTMVA";
+    TString fname_aggrTMVA = indir + label_aggrTMVA + "_mB.root";
 
-    TFile *fin_bdt = new TFile(rootf_bdt);
+    TFile *fin_aggrTMVA = new TFile(fname_aggrTMVA);
 
-    TH3D *hmB_bdt_3d = (TH3D *)fin_bdt->Get("hmBtag");
-    hmB_bdt_3d->SetName(TString(hmB_bdt_3d->GetName()) + "_bdt");
+    TH3D *hBtag_mB_ratio = (TH3D *)fin_aggrTMVA->Get("hBtag_mB_ratio");
+    hBtag_mB_ratio->SetName(TString(hBtag_mB_ratio->GetName()) + "_" + label_aggrTMVA);
 
     // ---- Find bins 
-    Int_t iyminLow = 1;
-    Int_t iymaxLow = hmB_truth_3d->GetYaxis()->FindBin(lowNvtxMax) - 1;
-
-    Int_t iyminHigh = hmB_truth_3d->GetYaxis()->FindBin(highNvtxMin);
-    Int_t iymaxHigh = hmB_truth_3d->GetYaxis()->GetNbins();
-
     // Int_t iyminLow = 1;
-    // Int_t iymaxLow = hmB_truth_3d->GetNbinsY();
+    // Int_t iymaxLow = hmB_truth_3d->GetYaxis()->FindBin(lowNvtxMax) - 1;
 
-    // Int_t iyminHigh = 1;
-    // Int_t iymaxHigh = hmB_truth_3d->GetNbinsY();
+    // Int_t iyminHigh = hmB_truth_3d->GetYaxis()->FindBin(highNvtxMin);
+    // Int_t iymaxHigh = hmB_truth_3d->GetYaxis()->GetNbins();
 
-    Int_t izmin = hmB_truth_3d->GetZaxis()->FindBin(ptMin);
-    Int_t izmax = hmB_truth_3d->GetZaxis()->FindBin(ptMax) - 1;
+    Int_t iyminLow = 1;
+    Int_t iymaxLow = hBtag_mB_ratio->GetNbinsY();
+
+    Int_t iyminHigh = 1;
+    Int_t iymaxHigh = hBtag_mB_ratio->GetNbinsY();
+
+    Int_t izmin = hBtag_mB_ratio->GetZaxis()->FindBin(ptMin);
+    Int_t izmax = hBtag_mB_ratio->GetZaxis()->FindBin(ptMax) - 1;
 
     Int_t ixmin = 1; // do not include underflow where mB > 0 and mBgen == 0
-    Int_t ixmax = hmB_truth_3d->GetXaxis()->GetNbins() + 1;
+    Int_t ixmax = hBtag_mB_ratio->GetXaxis()->GetNbins() + 1;
 
     // ---- Prepare plot
-    THStack *hmBStack = new THStack("hmBStack", "hmB");
-    hmBStack->SetTitle("; m_{pseudo-B, reco} / m_{pseudo-B, gen}; #frac{1}{N_{gen jets}} #frac{dN}{d m_{pseudo-B, reco} / m_{pseudo-B, gen}}");
+    THStack *hStack_mB_ratio = new THStack("hStack_mB_ratio", "hBtag_mB");
+    hStack_mB_ratio->SetTitle("; m_{pseudo-B, reco} / m_{pseudo-B, gen}; #frac{1}{N_{gen jets}} #frac{dN}{d m_{pseudo-B, reco} / m_{pseudo-B, gen}}");
 
-    TLegend *legmB = new TLegend(0.55, 0.6, 0.85, 0.9);
+    TLegend *legmB = new TLegend(0.55, 0.75, 0.85, 0.9);
     legmB->SetFillStyle(0);
     legmB->SetBorderSize(1);
     legmB->SetMargin(0.15);
 
     // ---- Make projections 
-    TH1D *hmB_truth_lowNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
-    normalize_histo(hmB_truth_lowNvtx, ixmin, ixmax);
-    format_histo(hmB_truth_lowNvtx, mykRed);
-    hmBStack->Add(hmB_truth_lowNvtx);
-    legmB->AddEntry(hmB_truth_lowNvtx, Form("truth, nvtx < %.0f", lowNvtxMax), "l");
+    // TH1D *hmB_truth_lowNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
+    // normalize_histo(hmB_truth_lowNvtx, ixmin, ixmax);
+    // format_histo(hmB_truth_lowNvtx, mykRed);
+    // hStack_mB_ratio->Add(hmB_truth_lowNvtx);
+    // legmB->AddEntry(hmB_truth_lowNvtx, Form("truth, nvtx < %.0f", lowNvtxMax), "l");
     // legmB->AddEntry(hmB_truth_lowNvtx, "truth", "l");
 
-    TH1D *hmB_truth_highNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
-    normalize_histo(hmB_truth_highNvtx, ixmin, ixmax);
-    format_histo(hmB_truth_highNvtx, mykRedLight);
-    hmBStack->Add(hmB_truth_highNvtx);
-    legmB->AddEntry(hmB_truth_highNvtx, Form("truth, nvtx > %.0f", highNvtxMin), "l");
+    // TH1D *hmB_truth_highNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
+    // normalize_histo(hmB_truth_highNvtx, ixmin, ixmax);
+    // format_histo(hmB_truth_highNvtx, mykRedLight);
+    // hStack_mB_ratio->Add(hmB_truth_highNvtx);
+    // legmB->AddEntry(hmB_truth_highNvtx, Form("truth, nvtx > %.0f", highNvtxMin), "l");
 
-    TH1D *hmB_bdt_lowNvtx = (TH1D *) hmB_bdt_3d->ProjectionX("hmB_bdt_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
-    normalize_histo(hmB_bdt_lowNvtx, ixmin, ixmax);
-    format_histo(hmB_bdt_lowNvtx, mykBlue);
-    hmBStack->Add(hmB_bdt_lowNvtx);
-    legmB->AddEntry(hmB_bdt_lowNvtx, Form("bdt, nvtx < %.0f", lowNvtxMax), "l");
-    // legmB->AddEntry(hmB_bdt_lowNvtx, "bdt", "l");
+    TH1D *hBtag_mB_ratio_lowNvtx = (TH1D *) hBtag_mB_ratio->ProjectionX("hBtag_mB_ratio_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
+    normalize_histo(hBtag_mB_ratio_lowNvtx, ixmin, ixmax);
+    format_histo(hBtag_mB_ratio_lowNvtx, mykBlue);
+    hStack_mB_ratio->Add(hBtag_mB_ratio_lowNvtx);
+    // legmB->AddEntry(hBtag_mB_ratio_lowNvtx, Form("bdt, nvtx < %.0f", lowNvtxMax), "l");
+    legmB->AddEntry(hBtag_mB_ratio_lowNvtx, "Detector / particle level, aggregated with TMVA", "l");
 
-    TH1D *hmB_bdt_highNvtx = (TH1D *) hmB_bdt_3d->ProjectionX("hmB_bdt_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
-    normalize_histo(hmB_bdt_highNvtx, ixmin, ixmax);
-    format_histo(hmB_bdt_highNvtx, mykBlueLight);
-    hmBStack->Add(hmB_bdt_highNvtx);
-    legmB->AddEntry(hmB_bdt_highNvtx, Form("bdt, nvtx > %.0f", highNvtxMin), "l");
+    TH1D *hBtag_mB_ratio_highNvtx = (TH1D *) hBtag_mB_ratio->ProjectionX("hBtag_mB_ratio_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
+    normalize_histo(hBtag_mB_ratio_highNvtx, ixmin, ixmax);
+    format_histo(hBtag_mB_ratio_highNvtx, mykBlueLight);
+    // hStack_mB_ratio->Add(hmB_bdt_highNvtx);
+    // legmB->AddEntry(hmB_bdt_highNvtx, Form("bdt, nvtx > %.0f", highNvtxMin), "l");
 
     // ---- Draw the histos
     TCanvas *cmB = new TCanvas("cmB", "", 1400, 800);
 
     cmB->cd();
-    hmBStack->Draw("histo nostack");
+    hStack_mB_ratio->Draw("histo nostack");
     legmB->Draw();
     cmB->Draw();
 
-    TString cname = Form("./plots/combined_mB_%.0f_nvtx_%.0f", lowNvtxMax, highNvtxMin) + suffix_bdt + ".png";
-    // TString cname = "./plots/combined_mB_no_nvtx_cut" + suffix_bdt + ".png";
+    TString cname = Form("./plots/combined_mB_%.0f_nvtx_%.0f.png", lowNvtxMax, highNvtxMin);
     cmB->Print(cname);
 }
--- myMacros/C106X/plot_mB.C
+++ myMacros/C106X/plot_mB.C
@@ -10,13 +10,16 @@
 void plot_mB()
 {
     // ---- Load tree
-    TString fdir = "/home/llr/cms/kalipoliti/C10630p1_miniAOD/src/HeavyIonsAnalysis/Configuration/test/rootf/";
-    TString label = "HiForestMiniAOD_HighPU_10000events_conmatch";
-    TString suffix = "_bdt44";
-    TString fname = label + suffix + ".root";
-    TString rootf = fdir + fname;
+    TString label = "aggrTMVA";
+    TString indir = "/data_CMS/cms/kalipoliti/ttbarMC/highPU/" + label + "/";
+    TString fin = indir + "merged_HiForestMiniAOD.root";
+    // TString fdir = "/home/llr/cms/kalipoliti/C10630p1_miniAOD/src/HeavyIonsAnalysis/Configuration/test/rootf/";
+    // TString label = "HiForestMiniAOD_HighPU_10000events_conmatch";
+    // TString suffix = "_bdt44_sig_vs_bkg";
+    // TString fname = label + suffix + ".root";
+    // TString rootf = fdir + fname;
 
-    tTree t = tTree(rootf);
+    tTree t = tTree(fin);
 
     std::vector<TString> activeBranches_t = {"nref", "nvtx",
                                              "jtHadFlav", "jtpt", "jteta", "jtphi", 
@@ -32,6 +35,11 @@ void plot_mB()
     Float_t x1min = 0.;
     Float_t x1max = 2.;
 
+    // mB
+    Int_t x2bins = 7*4;
+    Float_t x2min = 0.;
+    Float_t x2max = 7.;
+
     // nvtx
     Int_t y1bins = 20;
     Float_t y1min = 0.;
@@ -42,17 +50,23 @@ void plot_mB()
     Float_t z1min = 30.;
     Float_t z1max = 300.;
 
-    TH3D *hmB = new TH3D("hmB", "x=mBreco/mBgen, y=nvtx, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
-    TH3D *hmBtag = new TH3D("hmBtag", "x=mBreco/mBgen, y=nvtx, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
+    TH3D *hB_mB_ratio = new TH3D("hB_mB_ratio", "x=mBreco/mBgen, y=nvtx, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
+    TH3D *hBtag_mB_ratio = new TH3D("hBtag_mB_ratio", "x=mBreco/mBgen, y=nvtx, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
+
+    TH2D *hB_mB = new TH2D("hB_mB", "x=mBreco, y=jtpt", x2bins, x2min, x2max, y1bins, z1min, z1max);
+    TH2D *hBtag_mB = new TH2D("hBtag_mB", "x=mBreco, y=jtpt", x2bins, x2min, x2max, z1bins, z1min, z1max);
+
+    TH2D *hB_mB_gen = new TH2D("hB_mB_gen", "x=mB_gen, y=jtpt", x2bins, x2min, x2max, y1bins, z1min, z1max);
+    TH2D *hBtag_mB_gen = new TH2D("hBtag_mB_gen", "x=mB_gen, y=jtpt", x2bins, x2min, x2max, z1bins, z1min, z1max);
 
     Long64_t nentries = t.GetEntries();
     for (Long64_t ient = 0; ient < nentries; ient++) {
         t.GetEntry(ient);  
 
         // Print progress
-        if (ient % 1000 == 0) {
+        if (ient % 100000 == 0) {
             std::cout << "ient = " << ient << std::endl;
-            std::cout << "nref = " << t.nref << std::endl;
+            // std::cout << "nref = " << t.nref << std::endl;
         }
 
         for (int ijet = 0; ijet < t.nref; ijet++) {
@@ -87,17 +101,28 @@ void plot_mB()
             Int_t nvtx = t.nvtx;
             Float_t jtpt = t.jtpt[ijet];            
 
-            hmB->Fill(ratio, nvtx, jtpt);
-            if (passWP) hmBtag->Fill(ratio, nvtx, jtpt);
+            hB_mB_ratio->Fill(ratio, nvtx, jtpt);
+            hB_mB->Fill(mB, jtpt);
+            hB_mB_gen->Fill(mB_gen, jtpt);
+            if (passWP) {
+                hBtag_mB_ratio->Fill(ratio, nvtx, jtpt);
+                hBtag_mB->Fill(mB, jtpt);
+                hBtag_mB_gen->Fill(mB_gen, jtpt);
+            }
         } // end jet loop
     } // end entries loop
 
     // Save histograms 
-    TString rootf_out = "./histos/mB" + suffix + ".root";
+    TString rootf_out = "./histos/" + label + "_mB.root";
     TFile *fout = new TFile(rootf_out, "recreate");
     std::cout << "(Re)creating " << rootf_out << " file." << std::endl;
 
-    for (TH3D *h : {hmB, hmBtag}) {
+    for (TH3D *h : {hB_mB_ratio, hBtag_mB_ratio}) {
+        h->Write();
+    }
+
+    for (TH2D *h : {hB_mB, hBtag_mB,
+                    hB_mB_gen, hBtag_mB_gen}) {
         h->Write();
     }
 
