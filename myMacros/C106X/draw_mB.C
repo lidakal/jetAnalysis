4c009d0c5ea9526ef13ea986b6a8bb0c8f671fb8
Lida Kalipoliti <lethakal@gmail.com>
2023-02-24 13:27:57 +0100

    mB plots

--- myMacros/C106X/draw_mB.C
+++ myMacros/C106X/draw_mB.C
@@ -80,6 +80,10 @@ void draw_mB()
     TH3D *hBtag_mB_ratio = (TH3D *)fin_aggrTMVA->Get("hBtag_mB_ratio");
     hBtag_mB_ratio->SetName(TString(hBtag_mB_ratio->GetName()) + "_" + label_aggrTMVA);
 
+    TH2D *hBtag_mB = (TH2D *) fin_aggrTMVA->Get("hBtag_mB");
+    TH2D *hBtag_mB_gen = (TH2D *) fin_aggrTMVA->Get("hBtag_mB_gen");
+
+
     // ---- Find bins 
     // Int_t iyminLow = 1;
     // Int_t iymaxLow = hmB_truth_3d->GetYaxis()->FindBin(lowNvtxMax) - 1;
@@ -100,49 +104,77 @@ void draw_mB()
     Int_t ixmax = hBtag_mB_ratio->GetXaxis()->GetNbins() + 1;
 
     // ---- Prepare plot
-    THStack *hStack_mB_ratio = new THStack("hStack_mB_ratio", "hBtag_mB");
+    THStack *hStack_mB_ratio = new THStack("hStack_mB_ratio", "hBtag_mB_ratio");
     hStack_mB_ratio->SetTitle("; m_{pseudo-B, reco} / m_{pseudo-B, gen}; #frac{1}{N_{gen jets}} #frac{dN}{d m_{pseudo-B, reco} / m_{pseudo-B, gen}}");
 
-    TLegend *legmB = new TLegend(0.55, 0.75, 0.85, 0.9);
-    legmB->SetFillStyle(0);
-    legmB->SetBorderSize(1);
-    legmB->SetMargin(0.15);
+    TLegend *leg_mB_ratio = new TLegend(0.55, 0.75, 0.85, 0.9);
+    leg_mB_ratio->SetFillStyle(0);
+    leg_mB_ratio->SetBorderSize(1);
+    leg_mB_ratio->SetMargin(0.15);
+
+    THStack *hStack_mB = new THStack("hStack_mB", "hBtag_mB and hBtag_mB_gen");
+    hStack_mB->SetTitle("; m_{pseudo-B}; #frac{1}{N_{gen jets}} #frac{dN}{d m_{pseudo-B}}");
+
+    TLegend *leg_mB = new TLegend(0.55, 0.75, 0.85, 0.9);
+    leg_mB->SetFillStyle(0);
+    leg_mB->SetBorderSize(1);
+    leg_mB->SetMargin(0.15);
 
     // ---- Make projections 
     // TH1D *hmB_truth_lowNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
     // normalize_histo(hmB_truth_lowNvtx, ixmin, ixmax);
     // format_histo(hmB_truth_lowNvtx, mykRed);
     // hStack_mB_ratio->Add(hmB_truth_lowNvtx);
-    // legmB->AddEntry(hmB_truth_lowNvtx, Form("truth, nvtx < %.0f", lowNvtxMax), "l");
-    // legmB->AddEntry(hmB_truth_lowNvtx, "truth", "l");
+    // leg_mB_ratio->AddEntry(hmB_truth_lowNvtx, Form("truth, nvtx < %.0f", lowNvtxMax), "l");
+    // leg_mB_ratio->AddEntry(hmB_truth_lowNvtx, "truth", "l");
 
     // TH1D *hmB_truth_highNvtx = (TH1D *) hmB_truth_3d->ProjectionX("hmB_truth_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
     // normalize_histo(hmB_truth_highNvtx, ixmin, ixmax);
     // format_histo(hmB_truth_highNvtx, mykRedLight);
     // hStack_mB_ratio->Add(hmB_truth_highNvtx);
-    // legmB->AddEntry(hmB_truth_highNvtx, Form("truth, nvtx > %.0f", highNvtxMin), "l");
+    // leg_mB_ratio->AddEntry(hmB_truth_highNvtx, Form("truth, nvtx > %.0f", highNvtxMin), "l");
 
     TH1D *hBtag_mB_ratio_lowNvtx = (TH1D *) hBtag_mB_ratio->ProjectionX("hBtag_mB_ratio_lowNvtx", iyminLow, iymaxLow, izmin, izmax);
     normalize_histo(hBtag_mB_ratio_lowNvtx, ixmin, ixmax);
     format_histo(hBtag_mB_ratio_lowNvtx, mykBlue);
     hStack_mB_ratio->Add(hBtag_mB_ratio_lowNvtx);
-    // legmB->AddEntry(hBtag_mB_ratio_lowNvtx, Form("bdt, nvtx < %.0f", lowNvtxMax), "l");
-    legmB->AddEntry(hBtag_mB_ratio_lowNvtx, "Detector / particle level, aggregated with TMVA", "l");
+    // leg_mB_ratio->AddEntry(hBtag_mB_ratio_lowNvtx, Form("bdt, nvtx < %.0f", lowNvtxMax), "l");
+    leg_mB_ratio->AddEntry(hBtag_mB_ratio_lowNvtx, "Detector / particle level, aggregated with TMVA", "l");
 
     TH1D *hBtag_mB_ratio_highNvtx = (TH1D *) hBtag_mB_ratio->ProjectionX("hBtag_mB_ratio_highNvtx", iyminHigh, iymaxHigh, izmin, izmax);
     normalize_histo(hBtag_mB_ratio_highNvtx, ixmin, ixmax);
     format_histo(hBtag_mB_ratio_highNvtx, mykBlueLight);
     // hStack_mB_ratio->Add(hmB_bdt_highNvtx);
-    // legmB->AddEntry(hmB_bdt_highNvtx, Form("bdt, nvtx > %.0f", highNvtxMin), "l");
+    // leg_mB_ratio->AddEntry(hmB_bdt_highNvtx, Form("bdt, nvtx > %.0f", highNvtxMin), "l");
+
+    TH1D *hBtag_mB_gen_1d = (TH1D *) hBtag_mB_gen->ProjectionX("hBtag_mB_gen_1d", izmin, izmax);
+    format_histo(hBtag_mB_gen_1d, mykRed);
+    leg_mB->AddEntry(hBtag_mB_gen_1d, "Particle level", "l");
+    hStack_mB->Add(hBtag_mB_gen_1d);
+
+    TH1D *hBtag_mB_1d = (TH1D *) hBtag_mB->ProjectionX("hBtag_mB_1d", izmin, izmax);
+    format_histo(hBtag_mB_1d, mykBlue);
+    leg_mB->AddEntry(hBtag_mB_1d, "Detector level, aggregated with TMVA", "l");
+    hStack_mB->Add(hBtag_mB_1d);
 
     // ---- Draw the histos
-    TCanvas *cmB = new TCanvas("cmB", "", 1400, 800);
+    TCanvas *c_mB_ratio = new TCanvas("c_mB_ratio", "", 1400, 800);
 
-    cmB->cd();
+    c_mB_ratio->cd();
     hStack_mB_ratio->Draw("histo nostack");
-    legmB->Draw();
-    cmB->Draw();
+    leg_mB_ratio->Draw();
+    c_mB_ratio->Draw();
 
-    TString cname = Form("./plots/combined_mB_%.0f_nvtx_%.0f.png", lowNvtxMax, highNvtxMin);
-    cmB->Print(cname);
+    TString cname_mB_ratio = Form("./plots/combined_mB_ratio.png");
+    c_mB_ratio->Print(cname_mB_ratio);
+
+    TCanvas *c_mB = new TCanvas("c_mB", "", 1400, 800);   
+    c_mB->cd();
+    hStack_mB->Draw("histo nostack");
+    leg_mB->Draw();
+    c_mB->Draw();
+
+    TString cname_mB = "./plots/combined_mB.png";
+    c_mB->Print(cname_mB);
+    
 }
