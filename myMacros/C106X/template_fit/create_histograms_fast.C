b42c63d880df3093b1581b4c8b59227d11ed6cbe
Lida Kalipoliti <lethakal@gmail.com>
2023-06-25 16:58:05 +0200

    raw distributions data+mc comparison

---
+++ myMacros/C106X/template_fit/create_histograms_fast.C
@@ -0,0 +1,91 @@
+#include "../tTree.h"
+#include "../hist_utils.h"
+
+void create_histograms_fast()
+{
+    TString sample = "dijet";
+    TString label = "aggrTMVA_withHLT_v2_inclusive";
+    TString fin_name = "/data_CMS/cms/kalipoliti/compact_trees/" + sample + "_" + label + "_tree_template_fit.root";   
+    TString fout_name = "./histos/" + sample + "_" + label + "_histograms.root";
+
+    std::cout << "Creating file " << fout_name << std::endl;
+    TFile *fin = new TFile(fin_name);
+
+    Double_t jtpt;
+    Double_t logrg;
+    Double_t mb;
+    Double_t weight;
+
+    // jet pt bins : no underflow or overflow
+    const Int_t z1bins = 3;
+    Double_t z1min = 60.;
+    Double_t z1max = 120.; 
+
+    // ln(0.4/rg) bins : 1st bin untagged (unphysical)
+    const Int_t x1bins = 5;
+    Double_t x1min = -0.4; // if untagged or kT < cut : ln(R/Rg) = -0.2
+    Double_t x1max = 1.6; 
+
+    // mBch bins : 
+    const Int_t y1bins = 7;
+    Double_t y1min = 0.;
+    Double_t y1max = 7.;
+
+    std::vector<TString> tree_names; 
+    std::vector<TString> hist_names;
+    bool mc = false;
+    if (sample.Contains("data")) {
+        tree_names = {"tree_all"};
+        hist_names = {"h_data"};
+    } else {
+        mc = true;
+        tree_names = {"tree_sig", "tree_bkg_bb", "tree_bkg_rest"};
+        hist_names = {"h_sig", "h_bkg_bb", "h_bkg_rest"};
+    }
+
+    TFile *fout = new TFile(fout_name, "recreate");
+
+    for (size_t i = 0; i < tree_names.size(); i++) {
+        TString tree_name = tree_names[i];
+        TString hist_name = hist_names[i];
+
+        TTree *tree = (TTree *) fin->Get(tree_name);
+        tree->SetBranchAddress("jtpt", &jtpt);
+        tree->SetBranchAddress("logrg", &logrg);
+        tree->SetBranchAddress("mb", &mb);
+        tree->SetBranchAddress("weight", &weight);
+
+        TH3D *h_training = new TH3D(hist_name + "_training", "x=rg, y=mBch, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
+        TH3D *h_testing = new TH3D(hist_name + "_testing", "x=rg, y=mBch, z=jtpt", x1bins, x1min, x1max, y1bins, y1min, y1max, z1bins, z1min, z1max);
+
+        h_training->Sumw2();
+        h_testing->Sumw2();
+
+        if (!mc) h_training->SetName(hist_name);
+
+        Long64_t nentries = tree->GetEntries();
+        for (Long64_t ient = 0; ient < nentries; ient++) {
+            tree->GetEntry(ient);
+
+            // Check for logrg 
+            if (std::abs(logrg - (-0.2)) > 1e-3 && logrg < 0.) continue;
+            // Check for mb
+            if (mb < 0. || mb > 7.) continue;
+
+            if (mc) {
+                // weight = 1.; //debug
+                if (ient > (nentries / 2))
+                    h_training->Fill(logrg, mb, jtpt, weight);
+                else
+                    h_testing->Fill(logrg, mb, jtpt, weight);
+            } else {
+                h_training->Fill(logrg, mb, jtpt);
+            }
+        } // tree entry loop
+        h_training->Write();
+        if (mc) h_testing->Write();
+    } // tree loop
+
+    fout->Close();
+    delete fout;
+}
---
+++ myMacros/C106X/template_fit/draw_raw_poster.C
@@ -0,0 +1,202 @@
+#include "../myPalette.h"
+
+void draw_raw_poster()
+{
+    Float_t font_size = 32.;
+    gStyle->SetLegendTextSize(font_size);
+    gStyle->SetTextSize(font_size);
+    gStyle->SetLabelSize(font_size, "XYZ");
+    gStyle->SetTitleSize(font_size, "XYZ");
+    gStyle->SetErrorX(0.5);
+    // mykGreen = kTeal+4;
+
+
+    // Load data btag
+    TString label_data_btag = "aggrTMVA_v2";
+    TString fin_name_data_btag = "./histos/data_" + label_data_btag + "_histograms.root";
+    TFile *fin_data_btag = new TFile(fin_name_data_btag);
+    TH3D *h_data_btag = (TH3D *) fin_data_btag->Get("h_data")->Clone("h_data_btag");
+
+    // Load data inclusive
+    TString label_data_incl = "aggrTMVA_v2_inclusive";
+    TString fin_name_data_incl = "./histos/data_" + label_data_incl + "_histograms.root";
+    TFile *fin_data_incl = new TFile(fin_name_data_incl);
+    TH3D *h_data_incl = (TH3D *) fin_data_incl->Get("h_data")->Clone("h_data_incl");
+
+    // Load dijet MC btag
+    TString fin_name_mc_dijet_btag = "./histos/dijet_aggrTMVA_withHLT_v2_histograms.root";
+    TFile *fin_mc_dijet_btag = new TFile(fin_name_mc_dijet_btag);
+
+    TH3D *h_sig_training_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_sig_training")->Clone("h_sig_training_dijet_btag");
+    TH3D *h_bkg_bb_training_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_bkg_bb_training")->Clone("h_bkg_bb_training_dijet_btag");
+    TH3D *h_bkg_rest_training_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_bkg_rest_training")->Clone("h_bkg_rest_training_dijet_btag");
+
+    TH3D *h_sig_testing_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_sig_testing")->Clone("h_sig_testing_dijet_btag");
+    TH3D *h_bkg_bb_testing_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_bkg_bb_testing")->Clone("h_bkg_bb_testing_dijet_btag");
+    TH3D *h_bkg_rest_testing_dijet_btag = (TH3D *) fin_mc_dijet_btag->Get("h_bkg_rest_testing")->Clone("h_bkg_rest_testing_dijet_btag");
+
+    // Load dijet MC inclusive
+    TString fin_name_mc_dijet_incl = "./histos/dijet_aggrTMVA_withHLT_v2_inclusive_histograms.root";
+    TFile *fin_mc_dijet_incl = new TFile(fin_name_mc_dijet_incl);
+
+    TH3D *h_sig_training_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_sig_training")->Clone("h_sig_training_dijet_incl");
+    TH3D *h_bkg_bb_training_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_bkg_bb_training")->Clone("h_bkg_bb_training_dijet_incl");
+    TH3D *h_bkg_rest_training_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_bkg_rest_training")->Clone("h_bkg_rest_training_dijet_incl");
+
+    TH3D *h_sig_testing_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_sig_testing")->Clone("h_sig_testing_dijet_incl");
+    TH3D *h_bkg_bb_testing_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_bkg_bb_testing")->Clone("h_bkg_bb_testing_dijet_incl");
+    TH3D *h_bkg_rest_testing_dijet_incl = (TH3D *) fin_mc_dijet_incl->Get("h_bkg_rest_testing")->Clone("h_bkg_rest_testing_dijet_incl");
+
+    // Compile MC reco
+    TH3D *h_mc_btag = (TH3D *) h_sig_training_dijet_btag->Clone("h_mc");
+    h_mc_btag->Sumw2();
+    for (auto h : {h_sig_testing_dijet_btag,
+                   h_bkg_bb_training_dijet_btag, h_bkg_bb_testing_dijet_btag,
+                   h_bkg_rest_training_dijet_btag, h_bkg_rest_testing_dijet_btag}) {
+                    h_mc_btag->Add(h);
+                   } 
+
+    TH3D *h_mc_incl = (TH3D *) h_sig_training_dijet_incl->Clone("h_mc");
+    h_mc_incl->Sumw2();
+    for (auto h : {h_sig_testing_dijet_incl,
+                   h_bkg_bb_training_dijet_incl, h_bkg_bb_testing_dijet_incl,
+                   h_bkg_rest_training_dijet_incl, h_bkg_rest_testing_dijet_incl}) {
+                    h_mc_incl->Add(h);
+                   } 
+    
+    // Make projections 
+    Int_t nbins_rg = h_data_btag->GetNbinsX();
+    Int_t nbins_mb = h_data_btag->GetNbinsY();
+    Int_t nbins_pt = h_data_btag->GetNbinsZ();
+
+    TH1D *h_data_btag_rg = (TH1D *) h_data_btag->ProjectionX("h_data_btag_rg", 1, nbins_mb, 1, nbins_pt);
+    TH1D *h_data_btag_mb = (TH1D *) h_data_btag->ProjectionY("h_data_btag_mb", 1, nbins_rg, 1, nbins_pt);
+    TH1D *h_data_btag_pt = (TH1D *) h_data_btag->ProjectionZ("h_data_btag_pt", 1, nbins_rg, 1, nbins_mb);
+
+    TH1D *h_data_incl_rg = (TH1D *) h_data_incl->ProjectionX("h_data_incl_rg", 1, nbins_mb, 1, nbins_pt);
+    TH1D *h_data_incl_mb = (TH1D *) h_data_incl->ProjectionY("h_data_incl_mb", 1, nbins_rg, 1, nbins_pt);
+    TH1D *h_data_incl_pt = (TH1D *) h_data_incl->ProjectionZ("h_data_incl_pt", 1, nbins_rg, 1, nbins_mb);
+
+    TH1D *h_mc_btag_rg = (TH1D *) h_mc_btag->ProjectionX("h_mc_btag_rg", 1, nbins_mb, 1, nbins_pt);
+    TH1D *h_mc_btag_mb = (TH1D *) h_mc_btag->ProjectionY("h_mc_btag_mb", 1, nbins_rg, 1, nbins_pt);
+    TH1D *h_mc_btag_pt = (TH1D *) h_mc_btag->ProjectionZ("h_mc_btag_pt", 1, nbins_rg, 1, nbins_mb);
+
+    TH1D *h_mc_incl_rg = (TH1D *) h_mc_incl->ProjectionX("h_mc_incl_rg", 1, nbins_mb, 1, nbins_pt);
+    TH1D *h_mc_incl_mb = (TH1D *) h_mc_incl->ProjectionY("h_mc_incl_mb", 1, nbins_rg, 1, nbins_pt);
+    TH1D *h_mc_incl_pt = (TH1D *) h_mc_incl->ProjectionZ("h_mc_incl_pt", 1, nbins_rg, 1, nbins_mb);
+
+    // Normalize histograms
+    for (auto h : {h_data_btag_rg, h_data_btag_mb, h_data_btag_pt,
+                   h_data_incl_rg, h_data_incl_mb, h_data_incl_pt,
+                   h_mc_btag_rg, h_mc_btag_mb, h_mc_btag_pt,
+                   h_mc_incl_rg, h_mc_incl_mb, h_mc_incl_pt,
+                   }) {
+                    h->Scale(1/h->Integral("width"));
+                    // std::cout << "error bin 3 = " << h->GetBinError(3) << std::endl;
+                   }
+
+    // Fix untagged bin in rg 
+    h_data_btag_rg->SetBinContent(1, h_data_btag_rg->GetBinContent(1) / 10);
+    h_data_incl_rg->SetBinContent(1, h_data_incl_rg->GetBinContent(1) / 10);
+    h_mc_btag_rg->SetBinContent(1, h_mc_btag_rg->GetBinContent(1) / 10);
+    h_mc_incl_rg->SetBinContent(1, h_mc_btag_rg->GetBinContent(1) / 10);
+
+    // Format + draw histograms 
+    THStack *h_rg = new THStack("h_rg", "");
+    h_rg->SetTitle("; ln(0.4/R_{g}); 1/N_{jets} dN / d(ln(0.4/R_{g}))");
+    h_rg->SetMaximum(0.5);
+
+    TLegend *leg_rg = new TLegend(0.5, 0.7, 0.95, 0.9); 
+    leg_rg->SetFillStyle(0);
+
+    THStack *h_mb = new THStack("h_mb", "");
+    h_mb->SetTitle("; m_{B}^{ch}; 1/N_{jets} dN / d(m_{B}^{ch})");
+
+    TLegend *leg_mb = new TLegend(0.5, 0.7, 0.9, 0.85); 
+    leg_mb->SetFillStyle(0);
+
+    THStack *h_pt = new THStack("h_pt", "");
+    h_pt->SetTitle("; p_{T}^{jet}; 1/N_{jets} dN / d(p_{T}^{jet})");
+
+    TLegend *leg_pt = new TLegend(0.5, 0.7, 0.9, 0.85); 
+    leg_pt->SetFillStyle(0);
+
+    TCanvas *c_rg = new TCanvas("c_rg", "", 1200, 1000);
+
+    TCanvas *c_mb = new TCanvas("c_mb", "", 1200, 1000);
+
+    TCanvas *c_pt = new TCanvas("c_pt", "", 1200, 1000);
+
+    // Make decorations
+    TPaveText *info_top_left = new TPaveText(-0.4, 0.505, 0.05, 0.525, "nb ndc");
+    info_top_left->SetTextSize(font_size);
+    info_top_left->SetFillStyle(0);
+    info_top_left->SetLineWidth(0);
+    info_top_left->AddText("#bf{CMS} #it{Internal}");
+
+    TPaveText *info_top_right = new TPaveText(1., 0.505, 1.6, 0.525, "nb ndc");
+    info_top_right->SetTextSize(font_size);
+    info_top_right->SetFillStyle(0);
+    info_top_right->SetLineWidth(0);
+    info_top_right->AddText("#sqrt{s} = 5.02 TeV #it{pp}");
+
+    TPaveText *info_jets = new TPaveText(-0.4, 0.47, 0.05, 0.49, "nb ndc");
+    info_jets->SetTextSize(font_size);
+    info_jets->SetFillStyle(0);
+    info_jets->SetLineWidth(0);
+    // info_jets->AddText("b-tagged b-jets ");
+    // info_jets->AddText("80 < #it{p}_{T}^{jet} < 100 GeV");
+    info_jets->AddText("-2 < #it{#eta}^{jet} < 2, #it{k}_{T} > 1 GeV");
+    // info_jets->AddText("");
+    // info_jets->AddText("#it{k}_{T} > 1 GeV");
+
+    std::vector<THStack *> stacks = {h_rg, h_mb, h_pt};
+    std::vector<TLegend *> legends = {leg_rg, leg_mb, leg_pt};
+    std::vector<TH1D *> data_btag_hists = {h_data_btag_rg, h_data_btag_mb, h_data_btag_pt};
+    std::vector<TH1D *> data_incl_hists = {h_data_incl_rg, h_data_incl_mb, h_data_incl_pt};
+    std::vector<TH1D *> mc_btag_hists = {h_mc_btag_rg, h_mc_btag_mb, h_mc_btag_pt};
+    std::vector<TH1D *> mc_incl_hists = {h_mc_incl_rg, h_mc_incl_mb, h_mc_incl_pt};
+    std::vector<TCanvas *> canvases = {c_rg, c_mb, c_pt};
+
+    for (size_t i = 0; i < stacks.size(); i++) {
+        data_incl_hists[i]->SetMarkerStyle(kFullCircle);
+        data_incl_hists[i]->SetMarkerSize(2);
+        data_incl_hists[i]->SetMarkerColor(kBlack);
+        data_incl_hists[i]->SetLineColor(kBlack);
+        stacks[i]->Add(data_incl_hists[i], "pe");
+        legends[i]->AddEntry(data_incl_hists[i], "inclusive raw data", "pe");
+
+        data_btag_hists[i]->SetMarkerStyle(kFullDiamond);
+        data_btag_hists[i]->SetMarkerSize(3);
+        data_btag_hists[i]->SetMarkerColor(mykBlue);
+        data_btag_hists[i]->SetLineColor(mykBlue);
+        stacks[i]->Add(data_btag_hists[i], "pe");
+        legends[i]->AddEntry(data_btag_hists[i], "b-tagged raw data", "pe");
+
+        mc_incl_hists[i]->SetMarkerStyle(kFullTriangleDown);
+        mc_incl_hists[i]->SetMarkerSize(2);
+        mc_incl_hists[i]->SetMarkerColor(mykGreenLight);
+        mc_incl_hists[i]->SetLineColor(mykGreenLight);
+        stacks[i]->Add(mc_incl_hists[i], "pe");
+        legends[i]->AddEntry(mc_incl_hists[i], "inclusive MC", "pe");
+
+        mc_btag_hists[i]->SetMarkerStyle(kFullTriangleUp);
+        mc_btag_hists[i]->SetMarkerSize(2);
+        mc_btag_hists[i]->SetMarkerColor(mykRed);
+        mc_btag_hists[i]->SetLineColor(mykRed);
+        stacks[i]->Add(mc_btag_hists[i], "pe");
+        legends[i]->AddEntry(mc_btag_hists[i], "b-tagged MC", "pe");
+
+        
+        canvases[i]->cd();
+        stacks[i]->Draw("nostack");
+        legends[i]->Draw();
+        info_top_left->Draw();
+        info_top_right->Draw();
+        // info_jets->Draw();
+        canvases[i]->Draw();   
+
+        TString canvas_name = "./plots/raw_" + TString(canvases[i]->GetName()) + ".png";
+        canvases[i]->Print(canvas_name);
+    }
+}
