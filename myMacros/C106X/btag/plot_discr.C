789a5d8fa03f0066f1597b8db4e21d3bbbb5efe3
Lida Kalipoliti <lethakal@gmail.com>
2023-07-25 09:58:37 +0200

    update drawing of roc

--- myMacros/C106X/btag/plot_discr.C
+++ myMacros/C106X/btag/plot_discr.C
@@ -2,16 +2,19 @@
 
 void plot_discr()
 {
-    // TString fin = "/data_CMS/cms/kalipoliti/aod/qcdMC/chargedSJ/merged_HiForestAOD.root";
-    TString fin = "/data_CMS/cms/kalipoliti/qcdMC/dijet/aggrTMVA_withNb/merged_HiForestMiniAOD.root";
-    TString label = "mini";
+    TString fin = "/data_CMS/cms/kalipoliti/aod/qcdMC/chargedSJ/merged_HiForestAOD.root";
+    // TString fin = "/data_CMS/cms/kalipoliti/qcdMC/dijet/aggrTMVA_withHLT_v2/merged_HiForestMiniAOD.root";
+    // TString fin = "/data_CMS/cms/kalipoliti/qcdMC/bjet/aggrTMVA_withHLT/merged_HiForestMiniAOD.root";
+    TString label = "aod";
     tTree t = tTree(fin);
 
     std::vector<TString> activeBranches_t = {"nref", 
-                                             "refeta", "refpt", "jtHadFlav", "jtNbHad",
+                                             "refeta", "refpt", "jtNbHad", "jtNcHad",
+                                             "jteta", "jtpt",
                                              "discr_deepFlavour_b", "discr_deepFlavour_bb", "discr_deepFlavour_lepb",
                                              "jtDiscDeepFlavourB", "jtDiscDeepFlavourBB", "jtDiscDeepFlavourLEPB",
-                                             "weight"
+                                             "weight",
+                                             "HLT_HIAK4CaloJet60_v1"
                                              };
                                              
     t.SetBranchStatus("*", 0);
@@ -38,26 +41,31 @@ void plot_discr()
     TH2F *hBB_discr = new TH2F("hBB_discr", "x=b+lepb, y=refpt", x1bins, x1min, x1max, z1bins, z1min, z1max);
 
     std::cout << "Creating histograms ..." << std::endl;
+
     Long64_t nentries = t.GetEntries();
-    for (Long64_t ient = 0; ient < nentries; ient++) {
+    Long64_t total_events = 20000000;
+    if (total_events > nentries) total_events = nentries;
+
+    double sumWeight = 0;
+    double sumWeightBtag = 0.;
+
+    for (Long64_t ient = 0; ient < total_events; ient++) {
         t.GetEntry(ient);  
+        double weight = t.weight;
 
         // Print progress
         if (ient % 1000000 == 0) {
             std::cout << "ient = " << ient << std::endl;
-            // std::cout << "nref = " << t.nref << std::endl;
-            // std::cout << "weight = " << t.weight << std::endl;
         }
 
-        // Choose nb of events
-        const Long64_t total_events = 10000000;
-        // const Long64_t total_events = 1;
-        if (ient > total_events) break;
-		// if (ient > 100) break;
+        if (label == "mini" && t.HLT_HIAK4CaloJet60_v1 != 1) continue;
+
+        // debug: test efficiency with cut on weight
+        if (weight > 0.2) continue;
 
         for (int ijet = 0; ijet < t.nref; ijet++) {
             // universal eta cut
-            if (std::abs(t.refeta[ijet]) > 2) continue;
+            if (std::abs(t.jteta[ijet]) > 2) continue;
 
             double discr_b = 0.;
             double discr_b_vs_bb = 0.;
@@ -69,26 +77,35 @@ void plot_discr()
                 discr_b_vs_bb = t.discr_deepFlavour_b[ijet] + t.discr_deepFlavour_lepb[ijet];
             } 
 
-            int flav = (int) t.jtHadFlav[ijet];
+            Int_t jtNbHad = t.jtNbHad[ijet];
+            Int_t jtNcHad = t.jtNcHad[ijet];
+            Float_t jtpt = t.jtpt[ijet];
 
-            if (flav == 5) {
-                hB_discr->Fill(discr_b, t.refpt[ijet], t.weight);
-                if (t.jtNbHad[ijet] == 1) {
-                    hSingleB_discr->Fill(discr_b_vs_bb, t.refpt[ijet], t.weight);
+            if (jtNbHad > 0) {
+                // hB_discr->Fill(discr_b, jtpt, weight);
+                if (jtNbHad == 1) {
+                    hSingleB_discr->Fill(discr_b_vs_bb, jtpt, weight);
+                    if (jtpt>=60 && jtpt < 140) sumWeight += weight;
+                    if (discr_b_vs_bb >= 0.7 && jtpt>=60 && jtpt < 140) sumWeightBtag += weight;
                 } else {
-                    hBB_discr->Fill(discr_b_vs_bb, t.refpt[ijet], t.weight);
+                    hBB_discr->Fill(discr_b_vs_bb, jtpt, weight);
                 }
-            } else if (flav == 4) {
-                hC_discr->Fill(discr_b, t.refpt[ijet], t.weight);
-            } else if (flav == 0) {
-                hL_discr->Fill(discr_b, t.refpt[ijet], t.weight);
+            } else if (jtNcHad > 0) {
+                hC_discr->Fill(discr_b_vs_bb, jtpt, weight);
+            } else {
+                hL_discr->Fill(discr_b_vs_bb, jtpt, weight);
             }       
         } // end jet loop
     } // end entry loop
 
+    std::cout << "efficiency = " << sumWeightBtag/sumWeight << std::endl;
+
     TString outName = "./histos/" + label + "_discr.root";
     TFile *fout = new TFile(outName, "recreate");
-    for (auto h : {hB_discr, hSingleB_discr, hBB_discr,
+    for (auto h : {
+                //    hB_discr, 
+                   hSingleB_discr, 
+                   hBB_discr,
                    hL_discr,
                    hC_discr}) {
                     h->Write();
