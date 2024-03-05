841dceee7ad6cc8f3fe693554cc492cabf520929
Lida Kalipoliti <lethakal@gmail.com>
2024-03-05 18:11:50 +0100

    add rg and zg plots for inclusive

--- myMacros/C106X/unfolding/draw_response_inclusive.C
+++ myMacros/C106X/unfolding/draw_response_inclusive.C
@@ -1,6 +1,6 @@
 #include "../draw_utils.h"
 
-void draw_response_inclusive()
+void draw_response_inclusive(TString observable="rg")
 {
     // setTDRStyle();
 
@@ -8,22 +8,27 @@ void draw_response_inclusive()
     gStyle->SetPalette(57);
     gStyle->SetPaintTextFormat(".2f"); 
 
-    TString sample = "herwig_dijet";
+    TString xlabel;
+    if (observable=="rg") xlabel = "ln(0.4/R_{g})";
+    else if (observable=="zg") xlabel = "z_{g}";
+    else if (observable=="zpt") xlabel = "z";
+
+    TString sample = "dijet";
     TString label = "aggrTMVA_inclusive";
     TString split_option = "full";
     TString suffix = "_jer_nom_jec_nom";
     TString fin_name = "./histos/" + sample + "_" + label + "_response_" + split_option + suffix + ".root";   
 
     TFile *fin = new TFile(fin_name);
-    TH2D *h_training_purity_rgpt = (TH2D *) fin->Get("h_training_purity_rgpt");
-    TH2D *h_training_efficiency_rgpt = (TH2D *) fin->Get("h_training_efficiency_rgpt");
-    RooUnfoldResponse *response_rgpt = (RooUnfoldResponse *) fin->Get("response_rgpt");
+    TH2D *h_training_purity = (TH2D *) fin->Get("h_training_purity_"+observable+"pt");
+    TH2D *h_training_efficiency = (TH2D *) fin->Get("h_training_efficiency_"+observable+"pt");
+    RooUnfoldResponse *response = (RooUnfoldResponse *) fin->Get("response_"+observable+"pt");
 
     // Draw purity + efficiency 
-    for (auto h : {h_training_purity_rgpt, h_training_efficiency_rgpt}) {
+    for (auto h : {h_training_purity, h_training_efficiency}) {
         h->GetXaxis()->SetTitleSize(font_size);    
         h->GetXaxis()->SetLabelSize(font_size);
-        h->GetXaxis()->SetTitle("ln(0.4/R_{g})");
+        h->GetXaxis()->SetTitle(xlabel);
 
         h->GetYaxis()->SetTitleSize(font_size);    
         h->GetYaxis()->SetLabelSize(font_size);
@@ -33,29 +38,29 @@ void draw_response_inclusive()
     }
 
     TCanvas *c_purity = new TCanvas("c_purity", "purity", 200, 10, 800, 600);
-    h_training_purity_rgpt->GetZaxis()->SetTitle("Reconstruction purity");
-    h_training_purity_rgpt->Draw("colz texte");
-    drawHeaderHerwig();
+    h_training_purity->GetZaxis()->SetTitle("Reconstruction purity");
+    h_training_purity->Draw("colz texte");
+    drawHeaderSimulation();
     c_purity->Draw();
-    c_purity->Print("plots_an/"+sample+"_"+label+"_purity.png");
+    c_purity->Print("plots_an/"+sample+"_"+label+"_purity_"+observable+".png");
 
     TCanvas *c_efficiency = new TCanvas("c_efficiency", "efficiency", 800, 600);
-    h_training_efficiency_rgpt->GetZaxis()->SetTitle("Reconstruction efficiency");
-    h_training_efficiency_rgpt->Draw("colz texte");
-    drawHeaderHerwig();
+    h_training_efficiency->GetZaxis()->SetTitle("Reconstruction efficiency");
+    h_training_efficiency->Draw("colz texte");
+    drawHeaderSimulation();
     c_efficiency->Draw();
-    c_efficiency->Print("plots_an/"+sample+"_"+label+"_efficiency.png");
+    c_efficiency->Print("plots_an/"+sample+"_"+label+"_efficiency_"+observable+".png");
     
 
     // Draw the response matrix
-    Int_t nbins_rg = h_training_purity_rgpt->GetNbinsX(); 
-    Int_t nbins_pt = h_training_purity_rgpt->GetNbinsY(); 
+    Int_t nbins_rg = h_training_purity->GetNbinsX(); 
+    Int_t nbins_pt = h_training_purity->GetNbinsY(); 
 
-    TMatrixD response_matrix = response_rgpt->Mresponse();
+    TMatrixD response_matrix = response->Mresponse();
     TH2D *response_histogram = new TH2D(response_matrix);
     TCanvas *c_response = new TCanvas("c_response", "response", 800, 600);
-    response_histogram->GetXaxis()->SetTitle("Detector level ln(0.4/R_{g}) * p_{T}^{jet} bins");
-    response_histogram->GetYaxis()->SetTitle("Particle level ln(0.4/R_{g}) * p_{T}^{jet} bins");
+    response_histogram->GetXaxis()->SetTitle("Detector level "+xlabel+" * p_{T}^{jet} bins");
+    response_histogram->GetYaxis()->SetTitle("Particle level "+xlabel+" * p_{T}^{jet} bins");
     response_histogram->GetZaxis()->SetTitle("Migration probability");
     response_histogram->Draw("colz");
     for (int i = 1; i < nbins_pt; i++) {
@@ -70,11 +75,11 @@ void draw_response_inclusive()
         hline->SetLineWidth(2);
         hline->Draw();
     }
-    drawHeaderHerwig();
-    c_response->Print("plots_an/"+sample+"_"+label+"_response.png");
+    drawHeaderSimulation();
+    c_response->Print("plots_an/"+sample+"_"+label+"_response_"+observable+".png");
 
     // Print condition number
-    TDecompSVD *svd= new TDecompSVD(response_rgpt->Mresponse());  // response is a RooUnfold response object, svd is the singular value decomposition (SVD) matrix. the response->Mresponse() returns the normalized migration matrix
+    TDecompSVD *svd= new TDecompSVD(response->Mresponse());  // response is a RooUnfold response object, svd is the singular value decomposition (SVD) matrix. the response->Mresponse() returns the normalized migration matrix
     auto singular_values = svd->GetSig(); //this is a vector with the singular values, i.e., the diagonal elements of S. They are ordered from largest to smallest.
     double cond_number = singular_values.Max() / singular_values.Min();
     std::cout << "Largest value = " << singular_values.Max() 
