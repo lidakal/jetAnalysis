2bd8a544591c109e4e73d86ad21d810cc0a97ee6
Lida Kalipoliti <lethakal@gmail.com>
2024-03-07 14:08:04 +0100

    fits for b tagged jets pythia+herwig rg/zg/zpt

--- myMacros/C106X/template_fit/draw_fit_multiple.C
+++ myMacros/C106X/template_fit/draw_fit_multiple.C
@@ -4,24 +4,26 @@
 void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString jec_opt="nom")
 {
     TString suffix = "_jer_" + jer_opt + "_jec_" + jec_opt;
-    // current date/time based on current system
-    time_t now = time(0);
+    // // current date/time based on current system
+    // time_t now = time(0);
 
-    // convert now to string form
-    char* dt = ctime(&now);
+    // // convert now to string form
+    // char* dt = ctime(&now);
 
-    std::cout << "The local date and time is: " << dt << std::endl;
+    // std::cout << "The local date and time is: " << dt << std::endl;
 
-    Float_t text_size = 26.;
+    Float_t text_size = 18.;
     gStyle->SetTextSize(text_size);
     gStyle->SetLegendTextSize(text_size);
     gStyle->SetLabelSize(text_size, "XYZ");
     gStyle->SetTitleSize(text_size, "XYZ");
 
     TString option = "_glued";
-    TString sample = "aggrTMVA_XXT";
+    TString sample = "bjet";
+    TString label = "aggrTMVA_XXT";
     TString label_in = "data";
-    label_in += "_herwig_" + sample;
+    if (sample.Contains("herwig")) label_in += "_herwig_" + label;
+    else label_in += "_" + label;
     TString method = "RooFit";
     TString fin_name = "histos/fitted_parameters_" + method + "_" + label_in + "_" + observable + option + suffix + ".root";
     std::cout << "fin = " << fin_name << std::endl;
@@ -52,33 +54,47 @@ void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString
 
     Int_t choose_bin = 2;
     for (Int_t ibin_pt = choose_bin; ibin_pt <= choose_bin; ibin_pt++) {
-        TCanvas *c_mb = new TCanvas(Form("c_mb_%d", ibin_pt), "", 1600, 1000);
-        
-        TPad *pad1 = new TPad("pad1", "",0.0,0.0,0.33,0.2);
-        TPad *pad2 = new TPad("pad2", "",0.0,0.2,0.33,0.5);
-        TPad *pad3 = new TPad("pad3", "",0.0,0.5,0.33,0.7);
-        TPad *pad4 = new TPad("pad4", "",0.0,0.7,0.33,1.0);
-        TPad *pad5 = new TPad("pad5", "",0.33,0.0,0.66,0.2);
-        TPad *pad6 = new TPad("pad6", "",0.33,0.2,0.66,0.5);
-        TPad *pad7 = new TPad("pad7", "",0.33,0.5,0.66,0.7);
-        TPad *pad8 = new TPad("pad8", "",0.33,0.7,0.66,1.0);
-        TPad *pad9 = new TPad("pad9", "",0.66,0.5,0.99,0.7);
-        TPad *pad10 = new TPad("pad10", "",0.66,0.7,0.99,1.0);
-        TPad *pad11 = new TPad("pad11", "",0.66,0.0,0.99,0.5);
+        TCanvas *c_mb = new TCanvas(Form("c_mb_%d", ibin_pt), "", 1200, 1000);
 
-        std::vector<TPad *> dist_pads = {pad4, pad8, pad10, pad2, pad6};
-        std::vector<TPad *> ratio_pads = {pad3, pad7, pad9, pad1, pad5};
+        TPad *pad11 = new TPad("pad11", "",0.0,0.77,0.33,0.99);
+        TPad *pad12 = new TPad("pad12", "",0.33,0.77,0.66,0.99);
+        TPad *pad13 = new TPad("pad13", "",0.66,0.77,0.99,0.99);
+
+        TPad *pad21 = new TPad("pad21", "",0.0,0.44,0.33,0.66);
+        TPad *pad22 = new TPad("pad22", "",0.33,0.44,0.66,0.66);
+        TPad *pad23 = new TPad("pad23", "",0.66,0.44,0.99,0.66);
+
+        TPad *pad31 = new TPad("pad31", "",0.0,0.11,0.33,0.33);
+        TPad *pad32 = new TPad("pad32", "",0.33,0.11,0.66,0.33);
+        TPad *pad33 = new TPad("pad33", "",0.66,0.11,0.99,0.33);
+
+        TPad *pad111 = new TPad("pad111", "",0.0,0.66,0.33,0.77);
+        TPad *pad121 = new TPad("pad121", "",0.33,0.66,0.66,0.77);
+        TPad *pad131 = new TPad("pad131", "",0.66,0.66,0.99,0.77);
+
+        TPad *pad211 = new TPad("pad211", "",0.0,0.33,0.33,0.44);
+        TPad *pad221 = new TPad("pad221", "",0.33,0.33,0.66,0.44);
+        TPad *pad231 = new TPad("pad231", "",0.66,0.33,0.99,0.44);
+
+        TPad *pad311 = new TPad("pad311", "",0.0,0.,0.33,0.11);
+        TPad *pad321 = new TPad("pad321", "",0.33,0.,0.66,0.11);
+        TPad *pad331 = new TPad("pad331", "",0.66,0.,0.99,0.11);
+        
+
+        std::vector<TPad *> dist_pads = {pad11, pad12, pad13,
+                                         pad21, pad22, pad23,
+                                         pad31, pad32};
+
+        std::vector<TPad *> ratio_pads = {pad111, pad121, pad131,
+                                          pad211, pad221, pad231,
+                                          pad311, pad321};
 
         Double_t pt_min = h_sig_training_dijet->GetZaxis()->GetBinLowEdge(ibin_pt);
         Double_t pt_max = h_sig_training_dijet->GetZaxis()->GetBinUpEdge(ibin_pt);
 
         for (Int_t ibin_x = 1; ibin_x <= nbins_x; ibin_x++) {
-            if (ibin_x>5) break;
             int ipad = ibin_x - 1;
-
-            // if (ibin_x<=5) continue;
-            // int ipad = ibin_x - 6;
-
+            
             Double_t x_min = h_data->GetXaxis()->GetBinLowEdge(ibin_x);
             Double_t x_max = h_data->GetXaxis()->GetBinUpEdge(ibin_x);
 
@@ -122,13 +138,13 @@ void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString
             THStack *h_stack_mb = new THStack(Form("h_stack_mb_%d_%d", ibin_pt, ibin_x), "");
             h_stack_mb->SetTitle("; m_{B}^{ch}; entries");
 
-            TLegend *leg_mb = new TLegend(0.3, 0.3, 0.7, 0.6);
+            TLegend *leg_mb = new TLegend(0.3, 0.2, 0.7, 0.6);
             leg_mb->SetFillStyle(0);
             leg_mb->SetBorderSize(0);
             leg_mb->SetMargin(0.15);
             // leg_mb->SetHeader(Form("b tagged jets, k_{T} > 1 GeV"));
 
-            TPaveText *rg_range_text = new TPaveText(0.4, 0.7, 1., 0.8, "ndc");
+            TPaveText *rg_range_text = new TPaveText(0.5, 0.7, 1., 0.8, "ndc");
             rg_range_text->SetTextSize(text_size);
             rg_range_text->SetFillStyle(0);
             rg_range_text->SetBorderSize(0);
@@ -137,8 +153,10 @@ void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString
             if (observable=="rg") xlabel = "ln(#frac{0.4}{R_{g}})";
             else if (observable=="zg") xlabel = "z_{g}";
             else if (observable=="zpt") xlabel = "z";
-            if (ibin_x > 1) rg_range_text->AddText(Form("%.3f < %s < %.3f", x_min, xlabel.Data(), x_max));
-            else rg_range_text->AddText("fail SD or k_{T} < 1 GeV");
+
+            if (observable!="zpt" && ibin_x==1) rg_range_text->AddText("fail SD or k_{T} < 1 GeV");
+            else if (observable=="rg" && ibin_x==nbins_x) rg_range_text->AddText(Form("%.2f < %s", x_min, xlabel.Data()));
+            else rg_range_text->AddText(Form("%.2f < %s < %.2f", x_min, xlabel.Data(), x_max));
 
             // Format histos
             // for (auto h : {h_data_mb, h_sig_mb, h_bkg_bb_mb, h_bkg_rest_mb}) {
@@ -148,64 +166,77 @@ void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString
             h_data_mb->SetMarkerStyle(kFullCircle);
             h_data_mb->SetMarkerColor(kBlack);
             h_data_mb->SetMarkerSize(1);
-            leg_mb->AddEntry(h_data_mb, label_in, "pe1");
+            leg_mb->AddEntry(h_data_mb, "data", "pe1");
 
             h_sig_mb->Scale(ndata*sig_fraction/h_sig_mb->Integral(1, nbins_mb));
             h_sig_mb->SetFillStyle(1001);
             h_sig_mb->SetFillColor(kRed-7);
             h_sig_mb->SetMarkerStyle(1);
             h_stack_mb->Add(h_sig_mb);
-            leg_mb->AddEntry(h_sig_mb, "signal", "f");
+            leg_mb->AddEntry(h_sig_mb, "single b jets", "f");
 
             h_bkg_bb_mb->Scale(ndata*bkg_bb_fraction/h_bkg_bb_mb->Integral(1, nbins_mb));
             h_bkg_bb_mb->SetFillStyle(1001);
             h_bkg_bb_mb->SetFillColor(kBlue-3);
             h_bkg_bb_mb->SetMarkerStyle(1);
             h_stack_mb->Add(h_bkg_bb_mb);
-            leg_mb->AddEntry(h_bkg_bb_mb, "bb bkg", "f");
+            leg_mb->AddEntry(h_bkg_bb_mb, "bb jets", "f");
 
             h_bkg_rest_mb->Scale(ndata*bkg_rest_fraction/h_bkg_rest_mb->Integral(1, nbins_mb));
             h_bkg_rest_mb->SetFillStyle(1001);
             h_bkg_rest_mb->SetFillColor(kGreen-6);
             h_bkg_rest_mb->SetMarkerStyle(1);
             h_stack_mb->Add(h_bkg_rest_mb);
-            leg_mb->AddEntry(h_bkg_rest_mb, "light+c bkg", "f");
+            leg_mb->AddEntry(h_bkg_rest_mb, "gudsc jets", "f");
 
             // make sure the fractions are correct 
             double int0 = h_sig_mb->Integral(1, nbins_mb);
             double int1 = h_bkg_bb_mb->Integral(1, nbins_mb);
             double int2 = h_bkg_rest_mb->Integral(1, nbins_mb);
 
-            std::cout << "(" << ibin_pt << ", " << ibin_x << "):" << std::endl;
-            std::cout << "\tsignal fraction = " << int0/(int0+int1+int2) << std::endl;
-            std::cout << "\tbb fraction = " << int1/(int0+int1+int2) << std::endl;
-            std::cout << "\tlight+c fraction = " << int2/(int0+int1+int2) << std::endl;
+            // std::cout << "(" << ibin_pt << ", " << ibin_x << "):" << std::endl;
+            // std::cout << "\tsignal fraction = " << int0/(int0+int1+int2) << std::endl;
+            // std::cout << "\tbb fraction = " << int1/(int0+int1+int2) << std::endl;
+            // std::cout << "\tlight+c fraction = " << int2/(int0+int1+int2) << std::endl;
 
             dist_pads[ipad]->cd();
             // std::cout << "Top: " << dist_pads[ipad]->GetTopMargin() << std::endl;
             dist_pads[ipad]->SetTopMargin(0.15);
             dist_pads[ipad]->SetBottomMargin(0.);
-            dist_pads[ipad]->SetLeftMargin(0.15);
-            dist_pads[ipad]->SetRightMargin(0.08);
-            h_stack_mb->Draw("hist e1");
-            dist_pads[ipad]->Draw();
-            TAxis *yaxis = h_stack_mb->GetYaxis();
-            yaxis->SetTitleOffset(2.5);
-            // h_stack_mb->SetMaximum(0.45);
-            // h_stack_mb->SetMaximum(0.05);
+            dist_pads[ipad]->SetLeftMargin(0.2);
+            dist_pads[ipad]->SetRightMargin(0.05);
+
+            h_data_mb->Draw("pe1"); // draw once to have the correct scale
+            h_stack_mb->Draw("hist e1 same");
             h_data_mb->Draw("pe1 same");
             // leg_mb->Draw();
             rg_range_text->Draw();
+
+            if (ibin_x==1) {
+                TLatex *prelim = new TLatex;
+                prelim->SetNDC();
+                prelim->SetTextSize(20);
+                prelim->DrawLatex(0.185, 0.92, "#bf{CMS} #it{Work in Progress}");
+                prelim->Draw();
+            }
+
+            if (ibin_x==3) {
+                TLatex *lumi = new TLatex;
+                lumi->SetNDC();
+                lumi->SetTextSize(20);
+                lumi->DrawLatex(0.45, 0.92, "#bf{302 fb^{-1} (pp 5.02 TeV)}");
+                lumi->Draw();
+            }
 
             // Legend and jet info in empty pad
             TPaveText *info_jets = new TPaveText(0.3, 0.65, 0.7, 0.85);
             info_jets->SetTextSize(text_size);
             info_jets->SetFillStyle(0);
             info_jets->SetBorderSize(0);
-            info_jets->AddText("b tagged jets, k_{T} > 1 GeV");
+            // info_jets->AddText("b tagged jets, k_{T} > 1 GeV");
             info_jets->AddText(Form("%.0f < p_{T}^{jet} < %.0f (GeV)", pt_min, pt_max));
 
-            pad11->cd();
+            pad33->cd();
             leg_mb->Draw();
             info_jets->Draw();
 
@@ -231,60 +262,31 @@ void draw_fit_multiple(TString observable = "rg", TString jer_opt="nom", TString
             ratio_pads[ipad]->cd();
             ratio_pads[ipad]->SetTopMargin(0.);
             ratio_pads[ipad]->SetBottomMargin(0.4);
-            ratio_pads[ipad]->SetLeftMargin(0.15);
-            ratio_pads[ipad]->SetRightMargin(0.08);
+            ratio_pads[ipad]->SetLeftMargin(0.2);
+            ratio_pads[ipad]->SetRightMargin(0.05);
             // std::cout << ratio_pads[ipad]->GetLeftMargin() << std::endl;
             // std::cout << ratio_pads[ipad]->GetRightMargin() << std::endl;
             h_ratio->Draw("pe1");
-            h_ratio->GetYaxis()->SetTitleOffset(2.);
-            h_ratio->GetXaxis()->SetTitleOffset(4.5);
             line->Draw();
 
+            c_mb->cd();
+            dist_pads[ipad]->Draw();
+            ratio_pads[ipad]->Draw();
+            pad33->Draw();
 
-
-        }
-        // Make decorations
-        TPaveText *info_top_left = new TPaveText(0., 8.1, 4.1, 9.2, "nb ndc");
-        info_top_left->SetTextSize(text_size);
-        info_top_left->SetFillStyle(0);
-        info_top_left->SetLineWidth(0);
-        info_top_left->AddText("#bf{CMS} #it{Internal Simulation}");
-
-        TPaveText *info_top_right = new TPaveText(2.2, 0.72, 7., 0.8, "nb ndc");
-        info_top_right->SetTextSize(text_size);
-        info_top_right->SetFillStyle(0);
-        info_top_right->SetLineWidth(0);
-        info_top_right->AddText("PYTHIA8 #sqrt{s} = 5.02 TeV #it{pp}");
-
-        pad4->cd();
-        // info_top_left->Draw();
-
-        pad10->cd();
-        // info_top_right->Draw();
-
-        c_mb->cd();
-        pad1->Draw();
-        pad2->Draw();
-        pad3->Draw();
-        pad4->Draw();
-        pad5->Draw();
-        pad6->Draw();
-        pad7->Draw();
-        pad8->Draw();
-        pad9->Draw();
-        pad10->Draw();
-        pad11->Draw();
+            h_stack_mb->GetYaxis()->SetTitleOffset(5.);
+            h_ratio->GetYaxis()->SetTitleOffset(5.);
+            h_ratio->GetXaxis()->SetTitleOffset(5.5);
+        }      
         c_mb->Draw();
-
-        TString c_mb_name = Form("./plots/data_fit_mb_%.0f_pt_%.0f.png", pt_min, pt_max);
-        c_mb->Print(c_mb_name);
+        c_mb->Print("plots_an/"+sample+"_"+label+"_fits_"+observable+".png");
     }
 
-    // current date/time based on current system
-    time_t now2 = time(0);
+    // // current date/time based on current system
+    // time_t now2 = time(0);
 
-    // convert now to string form
-    char* dt2 = ctime(&now2);
+    // // convert now to string form
+    // char* dt2 = ctime(&now2);
 
-    std::cout << "The local date and time is: " << dt2 << std::endl;
+    // std::cout << "The local date and time is: " << dt2 << std::endl;
 }
