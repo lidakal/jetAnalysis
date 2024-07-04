06eeb572ff197a68a7776e8a80734ed041f2fc96
Lida Kalipoliti <lethakal@gmail.com>
2024-07-04 12:58:46 +0200

    total unc plots for inclusive jets

---
+++ myMacros/C106X/uncertanties/draw_unc_sym_inclusive.C
@@ -0,0 +1,308 @@
+#include "../draw_utils.h"
+#include "../colorblind_palette.h"
+
+void draw_unc_sym_inclusive(TString observable="rg")
+{
+    TString xlabel;
+    if (observable=="rg") xlabel = "ln(R/R_{g})";
+    else if (observable=="zg") xlabel = "z_{g}";
+    else if (observable=="zpt") xlabel = "z^{ch}=p_{T}^{B,ch}/p_{T}^{jet,ch}";
+
+    gStyle->SetCanvasPreferGL(kTRUE);
+    gStyle->SetErrorX(0.5);
+
+    TFile *fin_jer = new TFile("histos/"+observable+"_jer_unc_inclusive.root");
+    TFile *fin_jec = new TFile("histos/"+observable+"_jec_unc_src_inclusive.root");
+    TFile *fin_trk_inef = new TFile("histos/"+observable+"_trk_inef_inclusive.root");
+    TFile *fin_model_unc = new TFile("histos/"+observable+"_model_unc_inclusive.root");
+    TFile *fin_stat_unc = new TFile("histos/"+observable+"_stat_unc_inclusive.root");
+    TFile *fin_mc_stat_unc = new TFile("histos/"+observable+"_mc_stat_unc_inclusive.root");
+
+    int nbins_pt = 3;
+    
+    TCanvas *c_unc = new TCanvas("c_unc", "", 800,600);
+
+    TString fout_name = "histos/total_unc_incl_"+observable+".root";
+    std::cout << "Creating file: " << fout_name << std::endl;
+    TFile *fout = new TFile(fout_name, "recreate");
+
+    int ibin_pt = 2;
+
+    TH1D *h_stat_unc_rel = (TH1D *) fin_stat_unc->Get(Form("h_stat_unc_rel_%d", ibin_pt))->Clone(Form("h_stat_unc_rel_%d", ibin_pt));
+    h_stat_unc_rel->SetLineColor(kBlack);
+    h_stat_unc_rel->SetFillStyle(3244);
+    h_stat_unc_rel->SetFillColorAlpha(kBlack, 0.5);
+    h_stat_unc_rel->SetMarkerStyle(1);
+    h_stat_unc_rel->GetYaxis()->SetTitle("Relative uncertainty");
+    h_stat_unc_rel->GetYaxis()->SetTitleOffset(1.5);
+    h_stat_unc_rel->SetLineWidth(3);
+    h_stat_unc_rel->SetLineStyle(1);
+
+    TH1D *h_jer_up_rel = (TH1D *) fin_jer->Get(Form("h_unc_rel_%d", ibin_pt))->Clone(Form("h_jer_up_rel_%d", ibin_pt));
+    h_jer_up_rel->SetMarkerStyle(kFullTriangleUp);
+    h_jer_up_rel->SetLineColor(cb_palette[0]);
+    h_jer_up_rel->SetLineWidth(3);
+    h_jer_up_rel->SetLineStyle(2);
+
+    TH1D *h_jec_up_rel = (TH1D *) fin_jec->Get(Form("h_unc_rel_%d", ibin_pt))->Clone(Form("h_jec_up_rel_%d", ibin_pt));
+    h_jec_up_rel->SetLineColor(cb_palette[1]);
+    h_jec_up_rel->SetMarkerStyle(kFullTriangleDown);
+    h_jec_up_rel->SetLineWidth(3);
+    h_jec_up_rel->SetLineStyle(3);
+
+    TH1D *h_mc_stat_unc_up_rel = (TH1D *) fin_mc_stat_unc->Get(Form("h_mc_stat_unc_up_rel_%d", ibin_pt))->Clone(Form("h_mc_stat_unc_up_rel_%d", ibin_pt));
+    h_mc_stat_unc_up_rel->SetLineColor(cb_palette[2]);
+    h_mc_stat_unc_up_rel->SetMarkerStyle(kFullCross);
+    h_mc_stat_unc_up_rel->SetLineWidth(3);
+    h_mc_stat_unc_up_rel->SetLineStyle(3);
+
+    TH1D *h_trk_inef_up_rel = (TH1D *) fin_trk_inef->Get(Form("h_inef_unc_rel_%d", ibin_pt))->Clone(Form("h_trk_inef_up_rel_%d", ibin_pt));
+    h_trk_inef_up_rel->SetLineColor(cb_palette[3]);
+    h_trk_inef_up_rel->SetMarkerStyle(kFullCrossX);
+    h_trk_inef_up_rel->SetLineWidth(3);
+    h_trk_inef_up_rel->SetLineStyle(4);    
+
+    TH1D *h_model_unc_rel = (TH1D *) fin_model_unc->Get(Form("h_model_unc_rel_%d", ibin_pt))->Clone(Form("h_model_unc_rel_%d", ibin_pt));
+    h_model_unc_rel->SetLineColor(cb_palette[4]);
+    h_model_unc_rel->SetMarkerStyle(kFullStar);
+    h_model_unc_rel->SetLineWidth(3);
+    h_model_unc_rel->SetLineStyle(5);
+
+    std::vector<TH1D *> histos_syst = {
+        h_jer_up_rel,
+        h_jec_up_rel,
+        h_mc_stat_unc_up_rel,
+        h_trk_inef_up_rel,
+        h_model_unc_rel, 
+    };
+    
+    std::vector<TH1D *> all_histos = {h_stat_unc_rel};
+    all_histos.insert(all_histos.end(), histos_syst.begin(), histos_syst.end());
+
+    // fix x axis
+    int nbins_x = h_stat_unc_rel->GetNbinsX();
+
+    int ibin_x_min = 2;
+    int ibin_x_max = nbins_x;
+
+    if (observable=="rg") ibin_x_max = nbins_x - 1;
+
+    for (auto h : all_histos) {
+        h->GetXaxis()->SetRange(ibin_x_min, ibin_x_max);
+    }
+
+    // ----------- SAVE TOTAL SYST ---------------
+    
+    TH1D *h_syst_unc_up = (TH1D *) h_stat_unc_rel->Clone(Form("h_syst_unc_up_%d", ibin_pt));
+    h_syst_unc_up->Reset();
+    for (int ibin_x = 1; ibin_x <= h_syst_unc_up->GetNbinsX(); ibin_x++) {
+        double syst_unc_up = 0;
+
+        for (auto h : histos_syst) {
+            double unc_h = std::abs(h->GetBinContent(ibin_x));
+            h->SetBinContent(ibin_x, unc_h);
+            syst_unc_up += unc_h*unc_h;            
+        }
+
+        syst_unc_up = std::sqrt(syst_unc_up);
+        h_syst_unc_up->SetBinContent(ibin_x, syst_unc_up);
+    }
+    h_syst_unc_up->Write();
+
+    TH1D *h_syst_unc_down = (TH1D *) h_syst_unc_up->Clone(Form("h_syst_unc_down_%d", ibin_pt));
+    h_syst_unc_down->Scale(-1.);
+    h_syst_unc_down->Write();
+
+    // ------------------ SAVE TOTAL SYST+STAT -------------------
+
+    TH1D *h_total_unc_up = (TH1D *) h_stat_unc_rel->Clone(Form("h_total_unc_up_%d", ibin_pt));
+    h_total_unc_up->Reset();
+    for (int ibin_x = 1; ibin_x <= h_total_unc_up->GetNbinsX(); ibin_x++) {
+        double total_unc_up = 0;
+
+        for (auto h : all_histos) {
+            double unc_h = std::abs(h->GetBinContent(ibin_x));
+            h->SetBinContent(ibin_x, unc_h);
+            total_unc_up += unc_h*unc_h;            
+        }
+
+        total_unc_up = std::sqrt(total_unc_up);
+        h_total_unc_up->SetBinContent(ibin_x, total_unc_up);
+    }
+    h_total_unc_up->Write();
+
+    TH1D *h_total_unc_down = (TH1D *) h_total_unc_up->Clone(Form("h_total_unc_down_%d", ibin_pt));
+    h_total_unc_down->Scale(-1.);
+    h_total_unc_down->Write();
+
+
+    //-------------------- LEGENDS -----------
+
+    // For lines/symbols : top left, 2 cols
+    
+    // TLegend *leg = new TLegend(0.2, 0.62, 0.5, 0.87); // log scale
+    TLegend *leg = new TLegend(0.2, 0.45, 0.5, 0.65); // abs scale
+    if (observable=="zpt") leg = new TLegend(0.45, 0.5, 0.75, 0.75); // abs scale
+    leg->SetNColumns(2);
+    leg->SetColumnSeparation(0.25); 
+    leg->SetMargin(0.25);
+
+    leg->AddEntry(h_jer_up_rel, "JER", "pl");
+    leg->AddEntry(h_model_unc_rel, "model response", "pl");
+    leg->AddEntry(h_jec_up_rel, "JEC", "pl");
+    leg->AddEntry(h_mc_stat_unc_up_rel, "response stats", "pl");
+    leg->AddEntry(h_trk_inef_up_rel, "3% trk inef", "pl");
+    
+    
+
+    // For ? : bottom, 4 cols
+    // TLegend *leg = new TLegend(0.2, 0.2, 0.8, 0.35);
+    // leg->AddEntry(h_jer_up_rel, "JER", "l");
+    // leg->AddEntry(h_model_unc_rel, "model response", "l");
+    // leg->AddEntry(h_trk_inef_up_rel, "3% trk inef", "l");
+    // leg->AddEntry(h_cl_frac_up_rel, "c+l fraction", "l");
+
+    // leg->AddEntry(h_jec_up_rel, "JEC", "l");
+    // leg->AddEntry(h_model_fit_unc_rel, "model templates", "l");
+    // leg->AddEntry(h_mc_stat_unc_up_rel, "response stats", "l");
+    // leg->AddEntry(h_sf_up_rel, "btag eff", "l");
+
+    leg->AddEntry(h_stat_unc_rel, "statistical", "f");
+    // leg->AddEntry(h_total_unc_up, "total unc.", "f");
+
+    leg->SetFillStyle(0);
+    leg->SetBorderSize(0);
+
+    // for log
+    // double ymin=0.001;
+    // double ymax=1.;
+
+    // for abs
+    double ymin=0.;
+    double ymax=0.04;
+
+    for (auto h : {h_total_unc_up}) {
+        h->GetYaxis()->SetRangeUser(ymin, ymax);
+        h->SetFillColorAlpha(kGray,0.1);
+        h->SetLineColor(kGray);
+        h->SetMarkerColor(kGray);
+        h->SetMarkerStyle(1);
+        h->SetFillStyle(1001);
+        h->GetXaxis()->SetTitle(xlabel);
+        // h->Draw("hist same");
+    }
+
+    for (auto h : {h_stat_unc_rel,
+                    }) {
+        h->GetYaxis()->SetRangeUser(ymin, ymax);
+        // h->SetFillColor(h->GetLineColor());
+        h->SetFillColorAlpha(h->GetLineColor(), 0.05);
+        h->SetFillStyle(1001);
+        h->SetLineWidth(0);
+        h->SetLineColorAlpha(kBlack,0);
+        h->SetMarkerColor(h->GetLineColor());
+        h->GetXaxis()->SetTitle(xlabel);
+        h->Draw("hist same");
+    }
+
+    TLine *line = new TLine(h_jer_up_rel->GetXaxis()->GetBinLowEdge(ibin_x_min), 0, h_jer_up_rel->GetXaxis()->GetBinUpEdge(ibin_x_max), 0);
+    // line->SetLineStyle(kDashed);
+    // line->Draw();
+
+    for (int i=0; i<histos_syst.size(); i++) {
+        TH1D *h = histos_syst[i];
+        h->GetYaxis()->SetRangeUser(ymin, ymax);
+        h->SetMarkerColor(h->GetLineColor());
+        // h->SetLineWidth(1);
+        // h->SetLineStyle(kDashed);
+        h->SetMarkerSize(2);
+        h->GetYaxis()->SetTitle("Relative uncertainty");
+        h->GetYaxis()->SetTitleOffset(2);
+        h->Draw("pl hist same");
+    }
+    // h_stat_unc_rel->Draw("pe2 same");
+    leg->Draw();
+    drawHeader();
+
+    for (int ibin_x=1; ibin_x<ibin_x_max; ibin_x++) {
+        double x = h_jer_up_rel->GetXaxis()->GetBinUpEdge(ibin_x);
+        TLine *line_x = new TLine(x, ymin, x, ymax);
+        line_x->SetLineColor(kGray);
+        line_x->SetLineStyle(kDashed);
+        // line_x->Draw();
+    }
+
+    double x1 = h_jer_up_rel->GetXaxis()->GetBinLowEdge(ibin_x_max);
+    double x2 = h_jer_up_rel->GetXaxis()->GetBinUpEdge(ibin_x_max);
+    double y1 = ymin;
+    double y2 = ymax;
+    TPaveText *overflow = new TPaveText(x1, y1, x2, y2);
+    overflow->SetBorderSize(1);
+    overflow->SetFillStyle(3644);
+    auto overflow_txt = overflow->AddText("overflow");
+    overflow_txt->SetTextAngle(90);
+    overflow_txt->SetTextSize(20);
+    // if (observable=="rg") overflow->Draw();
+
+    // ----------------- JET INFO ------------
+
+    // bottom, 1 col
+    // TLatex *jet_info = new TLatex;
+    // jet_info->SetNDC();
+    // jet_info->SetTextSize(20);
+    // if (observable!="zpt") {
+    //     TLatex *jet_info = new TLatex;
+    //     jet_info->SetNDC();
+    //     jet_info->SetTextSize(20);
+    //     jet_info->DrawLatex(0.2, 0.33, "anti-k_{T}, R=0.4 single b jets");
+    //     jet_info->DrawLatex(0.2, 0.28, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+    //     jet_info->DrawLatex(0.2, 0.23, "Charged soft drop emissions");
+    //     jet_info->DrawLatex(0.2, 0.18, "z_{cut}=0.1, #beta=0, k_{T} > 1 GeV");
+    //     jet_info->Draw();
+    // } else {
+    //     jet_info->DrawLatex(0.2, 0.81, "anti-k_{T}, R=0.4 single b jets");
+    //     jet_info->DrawLatex(0.2, 0.76, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+    // }
+
+    // top, 1 col 
+    TLatex *jet_info = new TLatex;
+    jet_info->SetNDC();
+    jet_info->SetTextSize(20);
+    if (observable!="zpt") {
+        TLatex *jet_info = new TLatex;
+        jet_info->SetNDC();
+        jet_info->SetTextSize(20);
+        jet_info->DrawLatex(0.2, 0.83, "anti-k_{T}, R=0.4 inclusive jets");
+        jet_info->DrawLatex(0.2, 0.78, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+        jet_info->DrawLatex(0.2, 0.73, "Charged soft drop");
+        jet_info->DrawLatex(0.2, 0.68, "z_{cut}=0.1, #beta=0, k_{T} > 1 GeV");
+        jet_info->Draw();
+    } else {
+        jet_info->DrawLatex(0.45, 0.84, "anti-k_{T}, R=0.4 inclusive jets");
+        jet_info->DrawLatex(0.45, 0.79, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+    }
+
+    // bottom, 2 cols 
+    // TLatex *jet_info = new TLatex;
+    // jet_info->SetNDC();
+    // jet_info->SetTextSize(20);
+    // if (observable!="zpt") {
+    //     TLatex *jet_info = new TLatex;
+    //     jet_info->SetNDC();
+    //     jet_info->SetTextSize(20);
+    //     jet_info->DrawLatex(0.2, 0.24, "anti-k_{T}, R=0.4 inclusive jets");
+    //     jet_info->DrawLatex(0.2, 0.19, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+    //     jet_info->DrawLatex(0.55, 0.24, "Charged soft drop");
+    //     jet_info->DrawLatex(0.55, 0.19, "z_{cut}=0.1, #beta=0, k_{T} > 1 GeV");
+    //     jet_info->Draw();
+    // } else {
+    //     jet_info->DrawLatex(0.2, 0.81, "anti-k_{T}, R=0.4 inclusive jets");
+    //     jet_info->DrawLatex(0.2, 0.76, "100 < p_{T}^{jet} < 120 GeV, |#eta^{jet}| < 2");
+    // }
+
+
+    c_unc->Draw();
+    // c_unc->SetLogy();
+    c_unc->Print("plots_an/total_unc_incl_"+observable+".pdf");
+    c_unc->Print("plots_an/total_unc_incl_"+observable+".png");
+}
