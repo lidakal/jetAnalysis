2fce8b3f2c821022d15410d90c8c0fbaf1ff8022
Lida Kalipoliti <lethakal@gmail.com>
2025-06-25 09:24:48 +0200

    update stat unc scripts

--- myMacros/C106X/uncertanties/stat_unc.C
+++ myMacros/C106X/uncertanties/stat_unc.C
@@ -1,123 +1,178 @@
+void drawHeader(void) {
+    TLatex *prelim = new TLatex;
+    prelim->SetNDC();
+    prelim->SetTextSize(28);
+    prelim->SetTextAlign(12);
+    prelim->DrawLatex(0.15, 0.965, "#bf{CMS} #it{Private work}");
+
+    TLatex *lumi = new TLatex;
+    lumi->SetNDC();
+    lumi->SetTextSize(28);
+    lumi->SetTextAlign(32);
+    lumi->DrawLatex(0.95, 0.965, "pp 301^{} pb^{-1} (5.02 TeV)");
+}
+
 void stat_unc(TString observable="rg")
 {
-     // Setup 
-    Float_t text_size = 20.;
+    // Setup plot
+    Float_t text_size = 28.;
     gStyle->SetTextSize(text_size);
-    gStyle->SetLegendTextSize(text_size);
-    gStyle->SetLabelSize(text_size, "XYZ");
+    gStyle->SetLegendTextSize(text_size-4);
+    gStyle->SetLabelSize(text_size-4, "XYZ");
     gStyle->SetTitleSize(text_size, "XYZ");
     gStyle->SetErrorX(0.5);
 
     TString xlabel;
-    if (observable=="rg") xlabel = "ln(0.4/R_{g})";
+    if (observable=="rg") xlabel = "ln(R/R_{g})";
     else if (observable=="zg") xlabel = "z_{g}";
-    else if (observable=="zpt") xlabel = "z";
-    TString ylabel = "1/N dN/d" + xlabel;
+    else if (observable=="zpt") xlabel = "z_{b,ch}";
+    TString ylabel;
+    if (observable=="rg") ylabel = "1/N dN/dln(R/R_{g})";
+    else if (observable=="zg") ylabel = "1/N dN/dz_{g}";
+    else if (observable=="zpt") ylabel = "1/N dN/dz_{b,ch}";
 
-    TFile *fin_nom = new TFile("../unfolding/histos/aggrTMVA_XXT_unfolded_histograms_"+observable+"_jer_nom_jec_nom_withSF.root");
+    TString sample = "dijet_PF40";
+    TString label = "aggrTMVA_inclusive";
+    bool is_inclusive = label.Contains("inclusive");
+    TString suffix = (is_inclusive) ? "" : "_withSF";
+
+    TFile *fin_nom = new TFile("../unfolding/histos/"+sample+"_"+label+"_unfolded_histograms_"+observable+"_jer_nom_jec_nom"+suffix+".root");
     TH2D *h_nom = (TH2D *) fin_nom->Get("h_data_unfolded")->Clone("h_nom");
 
-    int nbins_x = h_nom->GetNbinsX();
-    int nbins_pt = h_nom->GetNbinsY();
+    // Make projections
+    Int_t ibin_pt = 2; 
+    Double_t pt_min = h_nom->GetYaxis()->GetBinLowEdge(ibin_pt);
+    Double_t pt_max = h_nom->GetYaxis()->GetBinUpEdge(ibin_pt);
+    TH1D *h_nom_1d = (TH1D *) h_nom->ProjectionX("h_nom_1d", ibin_pt, ibin_pt);
 
-    TCanvas *c_jer = new TCanvas("c_jer", "", 1600, 600);
-    TPad *pad11 = new TPad("pad11", "", 0., 0., 0.33, 0.35);
-    TPad *pad12 = new TPad("pad12", "", 0.33, 0., 0.66, 0.35);
-    TPad *pad13 = new TPad("pad13", "", 0.66, 0., 0.99, 0.35);
-    TPad *pad21 = new TPad("pad21", "", 0., 0.35, 0.33, 1.);
-    TPad *pad22 = new TPad("pad22", "", 0.33, 0.35, 0.66, 1.);
-    TPad *pad23 = new TPad("pad23", "", 0.66, 0.35, 0.99, 1.);
+    // Normalize
+    Int_t nbins_x = h_nom_1d->GetNbinsX();
+    Int_t ibin_x_min = 1;
+    Int_t ibin_x_max = nbins_x;
+    if (observable!="zpt") ibin_x_min = 2;
+    if (observable=="rg") ibin_x_max = nbins_x - 1;
 
-    std::vector<TPad *> bottom_pads = {pad11, pad12, pad13};
-    std::vector<TPad *> top_pads = {pad21, pad22, pad23};
-    
-    TFile *fout = new TFile("./histos/"+observable+"_stat_unc_XXT.root", "recreate");
-    
-    for (int ibin_pt = 1; ibin_pt <= nbins_pt; ibin_pt++) {
-        int ipad = ibin_pt - 1;
-
-        double pt_min = h_nom->GetYaxis()->GetBinLowEdge(ibin_pt);
-        double pt_max = h_nom->GetYaxis()->GetBinUpEdge(ibin_pt);
-
-        TLegend *leg;
-        if (observable=="rg") leg = new TLegend(0.25, 0.1, 0.65, 0.35);
-        if (observable=="zg") leg = new TLegend(0.6, 0.55, 0.9, 0.85);
-        if (observable=="zpt") leg = new TLegend(0.6, 0.55, 0.9, 0.85);
-        leg->SetFillStyle(0);
-        leg->SetBorderSize(0);
-        leg->SetMargin(0.15);
-        leg->SetHeader(Form("%.0f < p_{T}^{jet} < %.0f (GeV)", pt_min, pt_max));
-
-        // Make projections 
-        TH1D *h_nom_1d = (TH1D *) h_nom->ProjectionX(Form("h_nom_1d_%d", ibin_pt), ibin_pt, ibin_pt);
-
-        int ibin_x_min = 1;
-        int ibin_x_max = nbins_x;
-
-        h_nom_1d->SetMarkerStyle(kFullCircle); 
-        h_nom_1d->SetMarkerColor(kBlack);
-        h_nom_1d->SetLineColor(kBlack);
-        leg->AddEntry(h_nom_1d, "nominal", "pe1");
-
-        top_pads[ipad]->cd();
-        top_pads[ipad]->SetRightMargin(0.05);
-        top_pads[ipad]->SetLeftMargin(0.2);
-        top_pads[ipad]->SetBottomMargin(0.01);
-        for (auto h : {h_nom_1d}) {
-            h->GetXaxis()->SetRange(ibin_x_min, ibin_x_max);
-            h->Scale(1/h->Integral(), "width");
-            h->GetYaxis()->SetTitle(ylabel);
-            h->GetYaxis()->SetTitleOffset(2.5);
-            h->Draw("pe1 same");
-        }
-        leg->Draw();
-
-        // Extract uncertainties 
-        TH1D *h_stat_unc_up = (TH1D *) h_nom_1d->Clone(Form("h_stat_unc_%d", ibin_pt));
-        h_stat_unc_up->Reset();
-        h_stat_unc_up->SetTitle("statistical unc per bin");
-
-        for (int ibin_x = 1; ibin_x <= nbins_x; ibin_x++) {
-            h_stat_unc_up->SetBinContent(ibin_x, h_nom_1d->GetBinError(ibin_x));
-        }
-
-        TH1D *h_stat_unc_down = (TH1D *) h_stat_unc_up->Clone("h_stat_unc_down");
-        h_stat_unc_down->Scale(-1.);
-
-        bottom_pads[ipad]->cd();
-        bottom_pads[ipad]->SetTopMargin(0.01);
-        bottom_pads[ipad]->SetBottomMargin(0.35);
-        bottom_pads[ipad]->SetRightMargin(0.05);
-        bottom_pads[ipad]->SetLeftMargin(0.2);
-
-        double ymax = std::max({h_stat_unc_up->GetBinContent(h_stat_unc_up->GetMaximumBin()), h_stat_unc_down->GetBinContent(h_stat_unc_down->GetMaximumBin())});
-        double ymin = std::min({h_stat_unc_up->GetBinContent(h_stat_unc_up->GetMinimumBin()), h_stat_unc_down->GetBinContent(h_stat_unc_down->GetMinimumBin())});
-
-        if (ymin<0) ymin*=1.1;
-        else ymin*=0.9;
-
-        if (ymax<0) ymax*=0.9;
-        else ymax*=1.1;
-
-        h_stat_unc_up->GetYaxis()->SetRangeUser(ymin, ymax);
-        h_stat_unc_down->GetYaxis()->SetRangeUser(ymin, ymax);
-
-        h_stat_unc_up->Draw("hist");
-        h_stat_unc_down->Draw("hist same");
-
-        TLine *line = new TLine(h_stat_unc_up->GetXaxis()->GetBinLowEdge(ibin_x_min), 0, h_stat_unc_up->GetXaxis()->GetBinUpEdge(ibin_x_max), 0);
-        line->SetLineStyle(kDashed);
-        line->Draw();
-
-        c_jer->cd();
-        top_pads[ipad]->Draw();
-        bottom_pads[ipad]->Draw();
-
-        h_stat_unc_up->Write();
-
-        // save relative uncertainty 
-        TH1D *h_stat_unc_rel = (TH1D *) h_stat_unc_up->Clone(Form("h_stat_unc_rel_%d", ibin_pt));
-        h_stat_unc_rel->Divide(h_nom_1d);
-        h_stat_unc_rel->Write();
+    for (auto h : {h_nom_1d}) {
+        h->GetXaxis()->SetRange(ibin_x_min, ibin_x_max);
+        h->Scale(1/h->Integral(), "width");
     }
+
+    // Format histograms
+    h_nom_1d->SetMarkerStyle(kFullCrossX);
+    h_nom_1d->SetMarkerColor(kBlack);
+    h_nom_1d->SetLineColor(kBlack);
+    h_nom_1d->SetMarkerSize(2);
+
+    // Uncertainties
+    TH1D *h_unc_up = (TH1D *) h_nom_1d->Clone("h_unc_up");
+    h_unc_up->Reset();
+    for (int ibin_x = ibin_x_min; ibin_x <= ibin_x_max; ibin_x++) {
+        double stat_unc = h_nom_1d->GetBinError(ibin_x);
+        h_unc_up->SetBinContent(ibin_x, stat_unc);
+    }
+    TH1D *h_unc_down = (TH1D *) h_unc_up->Clone("h_unc_down");
+    h_unc_down->Scale(-1.);
+
+    TH1D *h_unc_up_rel = (TH1D *) h_unc_up->Clone("h_unc_up_rel");
+    h_unc_up_rel->Divide(h_nom_1d);
+
+    TH1D *h_unc_down_rel = (TH1D *) h_unc_down->Clone("h_unc_down_rel");
+    h_unc_down_rel->Divide(h_nom_1d);
+
+    TH1D *h_unc_rel_sym_up = (TH1D *) h_unc_up_rel->Clone("h_unc_rel_sym_up"); // already symmetric
+    h_unc_rel_sym_up->SetFillColor(kGray);
+    h_unc_rel_sym_up->SetFillStyle(1001);
+    h_unc_rel_sym_up->SetLineColor(kGray);
+    TH1D *h_unc_rel_sym_down = (TH1D *) h_unc_rel_sym_up->Clone("h_unc_rel_sym_down"); 
+    h_unc_rel_sym_down->Scale(-1.);
+
+    // Legend
+    TLegend *leg = new TLegend(0.5, 0.35, 0.9, 0.55);
+    if (observable=="zpt") leg = new TLegend(0.2, 0.3, 0.43, 0.5);
+    leg->SetFillStyle(0);
+    leg->SetBorderSize(0);
+    leg->SetMargin(0.15);
+    leg->AddEntry(h_nom_1d, "Nominal", "pe1");
+
+    // Draw
+    TCanvas *c_unc = new TCanvas("c_unc", "", 700, 600);
+    TPad *top_pad = new TPad("top_pad", "", 0., 0.33, 1., 1.);
+    TPad *bottom_pad = new TPad("top_pad", "", 0., 0., 1., 0.33);
+
+    top_pad->SetLeftMargin(0.15);
+    top_pad->SetRightMargin(0.05);
+    top_pad->SetTopMargin(0.09);
+    top_pad->SetBottomMargin(0.03);
+    bottom_pad->SetLeftMargin(0.15);
+    bottom_pad->SetRightMargin(0.05);
+    bottom_pad->SetTopMargin(0.04);
+    bottom_pad->SetBottomMargin(0.35);
+
+    top_pad->cd();
+    h_nom_1d->GetYaxis()->SetTitle(ylabel);
+    h_nom_1d->GetYaxis()->SetTitleSize(text_size);
+    h_nom_1d->GetYaxis()->SetLabelSize(text_size-4);
+    h_nom_1d->GetYaxis()->SetTitleOffset(1.3);
+    if (!is_inclusive&&observable=="rg") h_nom_1d->GetYaxis()->SetRangeUser(0., 1.5);
+    else if (is_inclusive&&observable=="rg") h_nom_1d->GetYaxis()->SetRangeUser(0., 2.);
+    else if (!is_inclusive&&observable=="zg") h_nom_1d->GetYaxis()->SetRangeUser(0., 10.);
+    else if (is_inclusive&&observable=="zg") h_nom_1d->GetYaxis()->SetRangeUser(0., 7.);
+    else if (observable=="zpt") h_nom_1d->GetYaxis()->SetRangeUser(0., 4.);
+    h_nom_1d->GetXaxis()->SetLabelOffset(10.); // kick it out of existence
+    h_nom_1d->GetXaxis()->SetTitleOffset(10.); // kick it out of existence
+    h_nom_1d->Draw("pe1");
+    leg->Draw();
+    drawHeader();
+    // Jets text
+    if (observable!="zpt") {
+        TLatex *jet_info = new TLatex;
+        jet_info->SetNDC();
+        jet_info->SetTextSize(text_size-4);
+        jet_info->SetTextAlign(32);
+        if (is_inclusive) jet_info->DrawLatex(0.9, 0.83, "anti-k_{T}, R = 0.4 inclusive jets"); 
+        else jet_info->DrawLatex(0.9, 0.83, "anti-k_{T}, R = 0.4 b jets");
+        jet_info->DrawLatex(0.9, 0.75, Form("%.0f < p_{T}^{jet} < %.0f GeV/c, |#eta^{jet}| < 2", pt_min, pt_max));
+        jet_info->DrawLatex(0.9, 0.67, "Soft drop (charged particles)");
+        jet_info->DrawLatex(0.9, 0.59, "z_{cut} = 0.1, #beta = 0, k_{T} > 1 GeV/c");
+    } else {
+        TLatex *jet_info = new TLatex;
+        jet_info->SetNDC();
+        jet_info->SetTextSize(text_size-4);
+        jet_info->SetTextAlign(12);
+        jet_info->DrawLatex(0.2, 0.83, "anti-k_{T}, R = 0.4 b jets");
+        jet_info->DrawLatex(0.2, 0.75, Form("%.0f < p_{T}^{jet} < %.0f GeV/c, |#eta^{jet}| < 2", pt_min, pt_max));
+    }
+
+    bottom_pad->cd();
+    h_unc_rel_sym_up->GetXaxis()->SetTitle(xlabel);
+    h_unc_rel_sym_up->GetXaxis()->SetTitleSize(text_size);
+    h_unc_rel_sym_up->GetXaxis()->SetLabelSize(text_size-4);
+    h_unc_rel_sym_up->GetXaxis()->SetTitleOffset(3.);
+    h_unc_rel_sym_up->GetYaxis()->SetTitle("#splitline{Relative}{uncertainty}");
+    h_unc_rel_sym_up->GetYaxis()->SetTitleSize(text_size);
+    h_unc_rel_sym_up->GetYaxis()->SetLabelSize(text_size-4);
+    h_unc_rel_sym_up->GetYaxis()->SetTitleOffset(1.4);
+    if (is_inclusive) h_unc_rel_sym_up->GetYaxis()->SetRangeUser(-0.2, 0.2);
+    else if (observable=="rg") h_unc_rel_sym_up->GetYaxis()->SetRangeUser(-0.4, 0.4);
+    else if (observable=="zg") h_unc_rel_sym_up->GetYaxis()->SetRangeUser(-0.2, 0.2);
+    else if (observable=="zpt") h_unc_rel_sym_up->GetYaxis()->SetRangeUser(-0.4, 0.4);
+    h_unc_rel_sym_up->GetYaxis()->SetNdivisions(-4);
+    h_unc_rel_sym_up->Draw("hist same");
+    h_unc_rel_sym_down->Draw("hist same");
+
+    c_unc->cd();
+    top_pad->Draw();
+    bottom_pad->Draw();
+    c_unc->Draw();
+
+    // Save histograms 
+    TString suffix_out = (is_inclusive) ? "inclusive" : "XXT";
+    TString fout_name = "histos/" + observable + "_stat_unc_" + suffix_out + ".root";
+    TFile *fout = new TFile(fout_name, "RECREATE");
+    h_nom_1d->Write("h_nom_1d");
+    h_unc_up->Write("h_unc_up");
+    h_unc_up_rel->Write("h_unc_up_rel");
+    h_unc_rel_sym_up->Write("h_unc_rel_sym_up");
+    h_unc_rel_sym_down->Write("h_unc_rel_sym_down");
+    fout->Close();
 }
